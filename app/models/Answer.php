aa7d1766b5f7548b6fe182ff5d5ec51ef8e34e7c
Andrei Chugunov <admin@pythagor.com>
2013-07-18 00:33:47 +0400

    Answers Sorting. Not Finished.

--- app/models/Answer.php
+++ app/models/Answer.php
@@ -17,7 +17,134 @@ class Answer extends \app\models\AppModel {
         return $answer->hits;
     }
 
+    /**
+     * Moving Answer one position up
+     *
+     * @param number $id Answer Id
+     * @return array Result
+     */
+    public static function moveUp($id) {
+        $res = array();
+        $res['error'] = false;
+        if ($answer = self::first($id)) {
+            $currentOrder = $answer->display_order;
+            if ($currentOrder == 1) {
+                $res['position'] = 1;
+                $res['error'] = 'alreadyTop';
+                return $res;
+            }
+            if ($prev = self::first(array('conditions' => array('display_order' => $currentOrder - 1, 'questioncategory_id' => $answer->questioncategory_id)))) {
+                $answer->display_order = $currentOrder - 1;
+                $prev->display_order = $currentOrder;
+                $answer->save();
+                $prev->save();
+                $res['position'] = $currentOrder - 1;
+                return $res;
+            }
+        }
+        $res['error'] = 'wrongId';
+        return $res;
+    }
 
+    /**
+     * Moving Answer one position down
+     *
+     * @param number $id Answer Id
+     * @return array Result
+     */
+    public static function moveDown($id) {
+        $res = array();
+        $res['error'] = false;
+        if ($answer = self::first($id)) {
+            $countInCategory = self::find('count', array('conditions' => array('questioncategory_id' => $answer->questioncategory_id)));
+            $currentOrder = $answer->display_order;
+            if ($currentOrder == $countInCategory) {
+                $res['position'] = $currentOrder;
+                $res['error'] = 'alreadyBottom';
+                return $res;
+            }
+            if ($next = self::first(array('conditions' => array('display_order' => $currentOrder + 1, 'questioncategory_id' => $answer->questioncategory_id)))) {
+                $answer->display_order = $currentOrder + 1;
+                $next->display_order = $currentOrder;
+                $answer->save();
+                $next->save();
+                $res['position'] = $currentOrder + 1;
+                return $res;
+            }
+        }
+        $res['error'] = 'wrongId';
+        return $res;
+    }
 
+    /**
+     * Moving Answer to thw top position
+     *
+     * @param number $id Answer Id
+     * @return array Result
+     */
+    public static function moveTop($id) {
+        $res = array();
+        $res['error'] = false;
+        if ($answer = self::first($id)) {
+            $currentOrder = $answer->display_order;
+            if ($currentOrder == 1) {
+                $res['position'] = 1;
+                $res['error'] = 'alreadyTop';
+                return $res;
+            }
+            $data = array(
+                'display_order' => 'display_order +1',
+            );
+            $conditions = array(
+                'questioncategory_id' => $answer->questioncategory_id,
+                'display_order' => 1,
+            );
+            self::update(array(
+                'display_order' => 'display_order +1',
+            ), array(
+                'questioncategory_id' => $answer->questioncategory_id,
+                'display_order' => 1,
+            ));
+            $answer->display_order = 1;
+            $answer->save();
+            $res['position'] = 1;
+            return $res;
+        }
+        $res['error'] = 'wrongId';
+        return $res;
+    }
 
+    /**
+     * Moving Answer to the bottom position
+     *
+     * @param number $id Answer Id
+     * @return array Result
+     */
+    public static function moveBottom($id) {
+        $res = array();
+        $res['error'] = false;
+        if ($answer = self::first($id)) {
+            $countInCategory = self::find('count', array('conditions' => array('questioncategory_id' => $answer->questioncategory_id)));
+            $currentOrder = $answer->display_order;
+            if ($currentOrder == $countInCategory) {
+                $res['position'] = $currentOrder;
+                $res['error'] = 'alreadyBottom';
+                return $res;
+            }
+            $data = array(
+                'display_order' => '-1',
+            );
+            $conditions = array(
+                'questioncategory_id' => $answer->questioncategory_id,
+                'display_order' => '> ' . $currentOrder,
+            );
+            self::update($data, $conditions);
+            $answer->display_order = $countInCategory;
+            $answer->save();
+            $res['position'] = $countInCategory;
+            return $res;
+        }
+        $res['error'] = 'wrongId';
+        return $res;
+    }
 }
