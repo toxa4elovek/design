730203d3b4222aa49c640d2e96765204a3db8be3
Dmitriy <nyudmitriy@gmail.com>
2013-08-20 14:25:13 +0400

    html chars fix

--- app/views/pitches/crowdsourcing.xml.php
+++ app/views/pitches/crowdsourcing.xml.php
@@ -14,7 +14,7 @@
         else: ?>http://www.godesigner.ru/img/fb_icon.jpg<?php endif?></photo>
         <city></city>
         <user-name><?= $pitch->user->first_name . ' ' . $pitch->user->last_name ?></user-name>
-        <short-description><?php if($pitch->private == 0):?><?php echo nl2br($pitch->description) ?><?php else:?>Это закрытый питч и вам нужно подписать соглашение о неразглашении!<?php endif?></short-description>
+        <short-description><?php if($pitch->private == 0):?><?php echo htmlspecialchars(nl2br($pitch->description)) ?><?php else:?>Это закрытый питч и вам нужно подписать соглашение о неразглашении!<?php endif?></short-description>
         <prize type="integer"><?= (int) $pitch->price ?></prize>
         <start-date><?=gmdate('Y-m-d H:i:s', strtotime($pitch->started))?> UTC</start-date>
         <start_unix_time type="integer"><?= strtotime($pitch->started) ?></start_unix_time>
