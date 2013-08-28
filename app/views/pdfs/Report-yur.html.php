6e513ee240b3184b8b1ab2237887675ddcc6b175
Andrei Chugunov <admin@pythagor.com>
2013-08-28 20:08:46 +0400

    Buh - Add Yur Report.

--- app/views/pdfs/Report-yur.html.php
+++ app/views/pdfs/Report-yur.html.php
@@ -3,14 +3,14 @@
             font-size: 13px;
             line-height: 25px;
             margin: 0 20px 0 30px;">
-<p style="margin: 0 0 20px 0;">Отчет ООО «КРАУД МЕДИА» об исполнении агентского поручения по агентскому договору
+<p style="margin: 20px 0;">Отчет ООО «КРАУД МЕДИА» об исполнении агентского поручения по агентскому договору
 № <?=$pitch->id?>, размещённому на сайте godesigner.ru.
 </p>
 
 <p style="margin: 20px 0;"><?=date('d.m.Y', strtotime($pitch->totalFinishDate));?><br />
 г. Санкт-Петербург<br />
 Общество с ограниченной ответственностью «КРАУД МЕДИА», именуемое в дальнейшем Агент, в лице генерального директора Федченко Максима Юрьевича, действующего на основании Устава, предоставляет,
-а <?=$bill->name?>, оплативший по банковской карте* с номером транзакции (РРН) <?=$transaction->RRN?> , именуемый в дальнейшем Принципал,
+а <?=$bill->name?>, именуемое в дальнейшем Принципал,
 принимает настоящий отчет об исполнении агентского поручения по договору, размещённому на сайте godesigner.ru со следующими результатами:
 </p>
 
@@ -24,28 +24,31 @@
 </p>
 
 <p style="margin: 20px 0;">3. Итого сумма расходов, поступивших от Принципала, составляет <?=$money->formatMoney($commission + $totalfees + $pitch->price, array('suffix' => ' руб. 00 коп.', 'dropspaces' => true))?> НДС не облагается в соответствии с главой 26.2 Налогового кодекса РФ.</p>
-
-<p style="margin: 20px 0;">Согласно условиям публичной оферты и отсутствием претензий, со стороны Принципала отчет Агента считается утвержденным автоматически, а поручение выполненным надлежащим образом.</p>
+<br />
 <table width="750" cellspacing="0" border="0" cellpadding="0">
     <tr>
-        <td width="220" style="border-bottom:1.5px solid #E8585D;">
-            <p style="font-family: Arial, sans-serif; font-size: 14px; line-height: 25px;">Отчет представил от Агента:<br /><br />
-            генеральный&nbsp;директор<br />
-            ООО&nbsp;«КРАУД&nbsp;МЕДИА»<br /><br />
+        <td colspan="3" width="360" valign="top">
+            <p style="font-family: Arial, sans-serif; font-size: 15px; line-height: 25px;">Отчет принял от принципала:</p></td>
+        <td width="30"></td>
+        <td colspan="3" width="360" valign="top">
+            <p style="font-family: Arial, sans-serif; font-size: 15px; line-height: 25px;">Отчет представил от Агента:<br /><br />
+            генеральный&nbsp;директор ООО&nbsp;«КРАУД&nbsp;МЕДИА»<br /><br />
             Федченко М. Ю.</p>
         </td>
-        <td width="30"></td>
-        <td width="220" style="border-bottom: 1.5px solid #E8585D; background:url('<?=LITHIUM_APP_PATH?>/extensions/helper/sign.png') bottom no-repeat;"></td>
-        <td width="30"></td>
-        <td width="220" height="151" style="border-bottom: 1.5px solid #E8585D; background:url('<?=LITHIUM_APP_PATH?>/extensions/helper/seal.png') bottom no-repeat;"></td>
     </tr>
     <tr>
-        <td width="220"><sup style="font-family: Arial, sans-serif; font-size: 12px; color: #999;">ФИО</sup></td>
+        <td width="200" height="180" valign="top" style="border-top: 1.5px solid #E8585D;"><sup style="font-family: Arial, sans-serif; font-size: 12px; color: #999;">ФИО</sup></td>
         <td width="30"></td>
-        <td width="220"><sup style="font-family: Arial, sans-serif; font-size: 12px; color: #999;">подпись</sup></td>
+        <td width="130" height="180" valign="top" style="border-top: 1.5px solid #E8585D;"><sup style="font-family: Arial, sans-serif; font-size: 12px; color: #999;">подпись</sup></td>
         <td width="30"></td>
-        <td width="220"><sup style="font-family: Arial, sans-serif; font-size: 12px; color: #999;">место печати</sup></td>
+        <td width="200" height="180" valign="top" align="right" style="border-top: 1.5px solid #E8585D;"><sup style="font-family: Arial, sans-serif; font-size: 12px; color: #999;">ФИО</sup></td>
+        <td width="30"></td>
+        <td width="130" height="180" valign="top" align="right" style="border-top: 1.5px solid #E8585D;"><sup style="font-family: Arial, sans-serif; font-size: 12px; color: #999;">подпись</sup></td>
+    </tr>
+    <tr>
+        <td  colspan="3" width="360" style="border-top: 1.5px solid #E8585D;"><sup style="font-family: Arial, sans-serif; font-size: 12px; color: #999;">место печати</sup></td>
+        <td width="30"></td>
+        <td  colspan="3" width="360" align="right" style="border-top: 1.5px solid #E8585D;"><sup style="font-family: Arial, sans-serif; font-size: 12px; color: #999;">место печати</sup></td>
     </tr>
 </table>
-<p style="font-family: Arial, sans-serif; font-size: 10px; color: #AAA;">* - Согласно договору о переводе средств по платёжным картам №54/12 с «Мастер-Банк» (ОАО)</p>
 </div>
