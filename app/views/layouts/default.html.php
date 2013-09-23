e84f5c3f61203eb54a85017f87251d6a7b72cd40
Andrei Chugunov <admin@pythagor.com>
2013-09-23 19:28:53 +0400

    Facebook - Remove Do not show anymore.

--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -215,7 +215,6 @@ echo '<!--' . $this->session->read('user.blogpost.date') . '-->';
     <div style="position: absolute; top: 460px; left: 25px; width: 652px; height: 10px; background: #454650; box-shadow: 0 5px 5px rgba(0,0,0,.4);"></div>
     <div style="position: relative; height: 70px; width: 100%; background: url('/img/go-social-header.png') no-repeat 18px 0px #282a34; box-shadow: inset 0 -1px 0 rgba(39,41,50,1);">
         <a class="close-request" style="float: right; color: rgb(100, 143, 164); font-size: 12px; padding-right: 17px; background: url('/img/closerequest.png') no-repeat right center; margin-top: 30px; margin-right: 20px;" href="#">закрыть</a>
-        <span style="float: right; font-size: 12px; padding-right: 28px; margin-top: 30px; margin-right: 28px; color: #454650; background: url('/img/hiderequest.png') no-repeat right center;">больше не показывать</span>
     </div>
     <div style="position: relative; height: 390px; background: url('/img/404_bg.png') top center no-repeat; background-size: cover; box-shadow: inset 0 1px 0 rgba(45,47,56,1), 0 5px 5px rgba(0,0,0,.4);">
         <!-- Start: VK -->
