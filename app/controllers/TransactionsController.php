1d9a205ab5183a990cf6f4d000951d64c31129fa
Andrei Chugunov <admin@pythagor.com>
2013-10-14 17:29:54 +0400

    Paymaster - Step 3. Remove Paymaster for Category = 10.

--- app/controllers/TransactionsController.php
+++ app/controllers/TransactionsController.php
@@ -23,6 +23,7 @@ class TransactionsController extends \app\controllers\AppController {
             $sign = $webgate->generateSign($data);
 		    $data['sign'] = $sign;
             Logger::write('debug', serialize($data));
+            $data['category'] = $pitch->category_id;
             return $data;
         }
 	}
