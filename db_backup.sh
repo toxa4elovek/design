4c7cc1c2e35a48d5971df91defe8986a7b6bd2a3
Andrei Chugunov <admin@pythagor.com>
2013-10-22 20:37:04 +0400

    Server - Exclude Table from Backup.

--- db_backup.sh
+++ db_backup.sh
@@ -9,7 +9,7 @@ archive_file=$(date +%Y-%m-%d_%H-%M-%S)
 # Generic dump function
 ########################
 function genmysql () {
-/usr/bin/nice -n15 /usr/bin/ionice -c3 /usr/bin/mysqldump -hlocalhost --quick --add-drop-table -u$db_user -p$db_pass $db_ \
+/usr/bin/nice -n15 /usr/bin/ionice -c3 /usr/bin/mysqldump -hlocalhost --quick --add-drop-table -u$db_user -p$db_pass $db_ $ignore \
 | gzip -c > $dest/$file\_$archive_file.sql.gz
 }
 
@@ -20,6 +20,7 @@ db_user='example_user';
 db_pass='example_password';
 db_='example_db';
 file='example_output_file_prefix';
+ignore="--ignore-table=$db_.table-to-ignore";
 genmysql
 
 ##########
@@ -29,4 +30,14 @@ db_user='next_user';
 db_pass='next_password';
 db_='next_db';
 file='next_output_file_prefix';
+genmysql
+
+##########
+# gdev
+##########
+db_user='gdev';
+db_pass='gdev';
+db_='gdev';
+file='gdev';
+ignore="--ignore-table=$db_.sendemails";
 genmysql
