df02037a501a680af5d7d2dd534b7df6c2ef7f34
Dmitriy Nyu <nyudmitriy@gmail.com>
2013-12-04 18:05:26 +0800

    fix link parsing with pipes (|) and style for brief description

--- app/views/pitches/details.html.php
+++ app/views/pitches/details.html.php
@@ -148,7 +148,7 @@ foreach($experts as $expert) {
                     <?php endif;?>
 
                     <h2 class="blueheading">Описание питча</h2>
-                    <p class="regular"><?=$this->brief->e($pitch->description)?></p>
+                    <p class="regular" style="word-break: break-word;"><?=$this->brief->e($pitch->description)?></p>
 
                     <div class="separator" style="width:620px"></div>
 
