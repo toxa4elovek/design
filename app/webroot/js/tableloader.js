7dc38883e2816d24bc1e98b929339a8a7e8cff08
Dmitriy Nyu <nyudmitriy@gmail.com>
2014-01-14 16:14:19 +0800

    bugfix

--- app/webroot/js/tableloader.js
+++ app/webroot/js/tableloader.js
@@ -156,7 +156,7 @@ function TableLoader() {
             }
 
             var imgForDraft = '';
-            if((object.user_id == $('#user_id').val()) && (object.status != 2)) {
+            if((object.user_id == $('#user_id').val()) && (object.awarded == '')) {
                 if (object.published == 1) {
                     imgForDraft = ' not-draft';
                 }
