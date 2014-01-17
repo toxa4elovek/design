429ce85eb7d746aaca714d507599e612501a7dda
Andrei Chugunov <admin@pythagor.com>
2014-01-17 15:47:26 +0400

    Closing - Check Payment Data.

--- app/webroot/js/users/step1.js
+++ app/webroot/js/users/step1.js
@@ -5,12 +5,11 @@ $(document).ready(function() {
         if($('.wrong-input').length > 0) {
             return false;
         }
-        if(($('input[name="cashintype"]:checked').data('pay') == 'cards') && ($('input[name="accountnum"]').val().length < 20)) {
-            alert('Введите номер своего счета, он должен содержать не меньше 20 символов!');
-            return false;
-        }else if (($('input[name="cashintype"]:checked').data('pay') == 'cards') && ($('input[name="inn"]').val().length != 12)) {
-            alert('Введите свой личный ИНН!');
-            return false;
+        if ($('input[name="cashintype"]:checked').data('pay') == 'cards') {
+            accountCheck();
+            if ($('.account-check').length > 0) {
+                return false;
+            }
         }
         var href = $(this).attr('href');
         $.post('/users/savePaymentData.json', $('#worker-payment-data').serialize(), function(response) {
@@ -67,4 +66,85 @@ $(document).ready(function() {
         }
     });
     
-})
+    $('input[name=bik], input[name=coraccount], input[name=accountnum], input[name="inn"]').on('blur', function() {
+        accountCheck();
+    });
+    
+    $(document).on('click', function() {
+        if ($('.account-check').hasClass('active')) {
+            $('.account-check').remove();
+        }
+    });
+    
+});
+
+function accountCheck() {
+    $('.account-check').remove();
+    var resultCor = (fn_checkKS($('input[name=coraccount]').val())) ? 1 : 0;
+    var resultAcc = (fn_checkRS($('input[name=accountnum]').val(), $('input[name=bik]').val())) ? 2 : 0;
+    var result = resultCor + resultAcc;
+    var message = '';
+    switch (result) {
+    case 0:
+        message = 'Неверно указан Счёт.<br>Неверно указан Корсчёт.<br>'
+        break;
+    case 1:
+        message = 'Неверно указан Счёт.<br>'
+            break;
+    case 2:
+        message = 'Неверно указан Корсчёт.<br>'
+            break;
+    default:
+        break;
+    }
+    var messageBik = (/^\d{9}$/.test($('input[name=bik]').val())) ? '' : 'Неверно указан БИК.<br>';
+    var messageInn = (/^\d{12}$/.test($('input[name="inn"]').val()) ) ? '' : 'Неверно указан ИНН.<br>';
+    message = messageBik + messageInn + message;
+    if (message) {
+        var el = $('<tr class="account-check"><td colspan="2">' + message + '</td></tr>');
+        el.appendTo($('#step1table')).animate({'opacity': 1}, 200, function() { $(this).addClass('active'); });
+    }
+}
+
+/*
+ * From http://javascript.ru/forum/misc/37373-funkciya-klyuchevaniya-scheta.html
+ */
+function fn_bank_account(Str)  
+{         
+    var result = false;
+    var Sum = 0;
+    if (Str == 0) {
+        return result;
+    }
+    
+    //весовые коэффициенты
+    var v = [7,1,3,7,1,3,7,1,3,7,1,3,7,1,3,7,1,3,7,1,3,7,1];
+    
+    for (var i = 0; i <= 22; i++) 
+    { 
+        //вычисляем контрольную сумму
+        Sum = Sum + ( Number(Str.charAt(i)) * v[i] ) % 10;
+    }
+    
+    //сравниваем остаток от деления контрольной суммы на 10 с нулём
+    if(Sum % 10 == 0)
+    {
+        result = true;
+    }
+        
+    return result;          
+}
+
+function fn_checkKS(Account)  
+{
+    return (/^\d{20}$/.test(Account));
+}
+
+/*
+Проверка правильности указания расчётного счёта:
+1. Для проверки контрольной суммы перед расчётным счётом добавляются три последние цифры БИКа банка.
+*/
+function fn_checkRS(Account,BIK)  
+{
+    return fn_bank_account(BIK.substr(-3,3)+Account);
+}
