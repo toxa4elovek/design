5f80e7c2a11863e946b9da1f232ad71857922bde
Dmitriy Nyu <nyudmitriy@gmail.com>
2014-01-21 14:40:12 +0800

    fix

--- app/views/elements/newrelic/newrelic_header.html.php
+++ app/views/elements/newrelic/newrelic_header.html.php
@@ -1,6 +1,6 @@
 <?php
 if (extension_loaded('newrelic')) {
-    echo newrelic_set_appname('GoDesigner');
+    newrelic_set_appname('GoDesigner');
     echo newrelic_get_browser_timing_header();
 }
 ?>
