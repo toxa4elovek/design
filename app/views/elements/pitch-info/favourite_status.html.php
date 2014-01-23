0f699692d78aa97e6484dcae63c9c2dda6356633
Dmitriy Nyu <nyudmitriy@gmail.com>
2014-01-23 13:40:10 +0800

    additional check for array at favourite element

--- app/views/elements/pitch-info/favourite_status.html.php
+++ app/views/elements/pitch-info/favourite_status.html.php
@@ -1,4 +1,4 @@
-<?php if(($this->session->read('user')) && (!in_array($pitch->id, $this->session->read('user.faves')))):?>
+<?php if(($this->session->read('user')) && (is_array($this->session->read('user.faves'))) && (!in_array($pitch->id, $this->session->read('user.faves')))):?>
     <div style="width:36px;height:36px;float:right;margin-right: 20px;">
         <a data-pitchid="<?=$pitch->id?>" id="fav" data-type="add" href="#" title="Добавить в избранное"><img class="fav-plus" alt="добавить в избранное" src="/img/plus 2.png"></a>
     </div>
