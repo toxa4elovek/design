ab529c6efbadfdc86c776ab65ff6a7e9765a6d22
Andrei Chugunov <admin@pythagor.com>
2014-01-24 22:44:03 +0400

    Design - New Dropzone. Layout.

--- app/views/pitches/upload.html.php
+++ app/views/pitches/upload.html.php
@@ -23,35 +23,30 @@
                 <?php endif?>
             </div>
 
-            <h1 class="largeHeading">Загрузить решение</h1>
-
             <form action="/pitches/upload/<?=$pitch->id?>.json" method="post" id="solution" class="add-pitch upload-form" enctype="multipart/form-data">
+                <div class="upload-dropzone">
+                    <input type="file" id="truebutton" multiple="multiple" name="solution[]" class="fileinput-button">
+                    <input id="fakebutton" type="button" class="button" value="Выберите файлы">
+                </div>
                 <div style="float:left; width: 627px;">
-                    <div class="uploadblock" style="float:left;width: 190px;position:relative">
-
-
-                        <input type="file" id="truebutton" multiple="multiple" name="solution[]" class="fileinput-button" style="display:block; opacity:0; position:relative;z-index:5; width: 190px; height: 50px;" />
-                        <input id="fakebutton" type="button" class="button" style="position:absolute;z-index: 4;top:0;left:0;" value="Загрузить файлы">
-                    </div>
-                    <div style="clear:both;margin-bottom:10;"></div>
                     <ul id="filelist" class="supplement" style="margin-top: 10px;">
-                        <li class="fakeinput" style=" padding-top: 1px; margin-left:0;">Файлы не выбраны</li>
+                        <li class="fakeinput" style=" padding-top: 1px; margin-left:0;"></li>
                     </ul>
                     <div class="group" style="margin-top: 10px;background:none;margin-bottom:0;">
                         <p>
-                            <label class="greyboldheader">Описание, концепция или примечание</label>
+                            <label class="greyboldheader">Опишите идею</label>
                             <textarea id="charzone" class="upload-textarea" style="height:130px; width: 557px;margin-top: 8px;" name="description"></textarea>
                         </p>
                     </div>
                 </div>
-                <div style="float:left; width: 180px;margin-right:20px; margin-bottom: 20px;"><p class="supplement">
-                Пожалуйста, ознакомьтесь с правилами участия в <a href="http://www.godesigner.ru/answers/view/37">питче</a>. Прикрепляемые за раз файлы должны соответствовать одной концепции, быть в формате JPEG/RGB, не больше 5 Мбт, 600*400px, или уменьшенный по ширине до 800px эскиз сайта, подробнее <a href="http://www.godesigner.ru/answers/view/40">тут</a>. Если ваше решение победит в питче, вы загрузите 100% макеты и запрашиваемые в брифе файлы.
+                <div style="float:left; width: 230px; margin-bottom: 20px; margin-top: 50px;"><p class="supplement">
+                Прикрепляемые за раз файлы должны соответствовать одной концепции, быть в формате JPEG/RGB, max 800px по ширине, &lt;&nbsp;5&nbsp;МБ. Если ваше решение победит в питче, вы загрузите запрашиваемые заказчиком рабочие файлы.
                 </div>
                 <div style="height:2px;clear:both;width:807px;background: url('/img/obnovleniya_line.jpg') repeat-x scroll 0 100% transparent; margin-bottom: 15px;"></div>
 
                 <div style="float:left; width: 627px;">
                     <div class="group" style="background: none;margin-bottom:0;">
-                            <div style="margin-bottom:20px;"  class="greyboldheader">Использование изображений</div>
+                            <div style="margin-bottom:20px;"  class="greyboldheader">Укажите источники изображений и шрифтов</div>
                             <label style="margin-bottom: 15px;" class="checkboxtext label-switch"><input type="radio" name="licensed_work" value="0">ВСЯ РАБОТА - РЕЗУЛЬТАТ МОЕГО ТРУДА</label>
                             <div id="fine" style="display:none; margin-bottom:10px;margin-left:12px;">
                                 <img src="/img/bravo.png" alt="Браво!">
@@ -68,14 +63,14 @@
                         </div>
                     </div>
                 </div>
-                <div style="float:left; width: 180px;margin-right:20px;">
-                <p class="supplement">Если вы использовали чужие изображения или фотографии из банков, пожалуйста, заявите об этом.</p>
+                <div style="float:left; width: 230px;">
+                <p class="supplement">Если вы использовали чужие изображения, платные шрифты или фотографии из банков, пожалуйста, заявите об этом.</p>
                 </div>
 
                 <div style="height:1px;clear:both;width:807px;background: url('/img/obnovleniya_line.jpg') repeat-x scroll 0 100% transparent; margin-bottom: 15px;"></div>
 
                 <div class="tos-container supplement">
-                    <label><input type="checkbox" name="tos" style="margin-right:5px;"/>Я прочитал(а) и выражаю безусловное согласие с условиями настоящего <a href="/docs/dogovor.pdf" style="text-decoration: none;">конкурсного соглашения</a>.</label>
+                    <label><input type="checkbox" name="tos" style="margin-right:5px;"/>Я прочитал и согласен с <a href="/docs/dogovor.pdf" style="text-decoration: none;">правилами и условиями</a> Go Designer</label>
                 </div>
 
 
