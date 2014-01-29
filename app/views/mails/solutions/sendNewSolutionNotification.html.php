8beab0157981a1eb05695f153217baf6f188d293
Dmitriy Nyu <nyudmitriy@gmail.com>
2014-01-29 19:53:12 +0800

    Bugfix

--- app/views/mails/solutions/sendNewSolutionNotification.html.php
+++ app/views/mails/solutions/sendNewSolutionNotification.html.php
@@ -1,8 +1,3 @@
-<?php
-var_dump($this->solution->renderImageUrl($solution->images['solution_galleryLargeSize']));
-die();
-?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
