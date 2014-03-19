1733fc16e9dc7432e1f1170ccaec329b217979e0
Dmitriy Nyu <nyudmitriy@gmail.com>
2014-03-19 15:13:51 +0800

    Referal - emails changed for referal payment command

--- app/extensions/command/ReferalPayments.php
+++ app/extensions/command/ReferalPayments.php
@@ -8,12 +8,15 @@ use \app\extensions\mailers\SpamMailer;
 class ReferalPayments extends \app\extensions\command\CronJob {
 
     protected $dest = 'nyudmitriy@godesigner.ru';
+    protected $admin = 'fedchenko@godesigner.ru';
 
     public function run() {
         $this->header('Welcome to the ReferalPayments command!');
         $count = User::getReferalPayments();
         if ($count > 0) {
             $data = array('to' => $this->dest);
+            Spammailer::referalpayments($data);
+            $data = array('to' => $this->admin);
             Spammailer::referalpayments($data);
         }
         $this->out($count . ' users');
