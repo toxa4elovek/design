6096fbaa2f2d9c4403caff85edcfe9ecfaa80927
Влад <vladbar.job@yandex.ru>
2014-08-18 19:24:28 +1000

    Refactor - fix Pitch::getQuerySearchTerm

--- app/tests/cases/models/PitchTest.php
+++ app/tests/cases/models/PitchTest.php
@@ -228,9 +228,9 @@ class PitchTest extends AppUnit {
 	}
 	
 	public function testGetQuerySearchTerm() {
-		$this->assertEqual(array(), Pitch::getQuerySearchTerm());
 		$this->assertEqual(array(), Pitch::getQuerySearchTerm(array()));
-		$this->assertEqual(array('Pitch.title' => array('REGEXP' => 'тест тест тест|Тест тест тест|ТЕСТ ТЕСТ ТЕСТ')),Pitch::getQuerySearchTerm('Тест тест тест'));		$this->assertEqual(array('Pitch.title' => array('REGEXP' => 'тест тест тест|Тест тест тест|ТЕСТ ТЕСТ ТЕСТ')),Pitch::getQuerySearchTerm('тест тест тест'));
+		$this->assertEqual(array('Pitch.title' => array('REGEXP' => 'тест тест тест|Тест тест тест|ТЕСТ ТЕСТ ТЕСТ')),Pitch::getQuerySearchTerm('Тест тест тест'));
+		$this->assertEqual(array('Pitch.title' => array('REGEXP' => 'тест тест тест|Тест тест тест|ТЕСТ ТЕСТ ТЕСТ')),Pitch::getQuerySearchTerm('тест тест тест'));
 		$this->assertEqual(array('Pitch.title' => array('REGEXP' => 'test test test|Test test test|TEST TEST TEST')),Pitch::getQuerySearchTerm('test test test'));
         $this->assertEqual(array(), Pitch::getQuerySearchTerm(array('test' => 'test')));
         $this->assertEqual(array(), Pitch::getQuerySearchTerm(null));
