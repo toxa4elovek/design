2e24375d7f2885701ec763fd3790c92885bed60c
Влад <vladbar.job@yandex.ru>
2014-08-27 22:37:34 +1000

    Users - fix email popup in registration form

--- app/webroot/js/users/registration.js
+++ app/webroot/js/users/registration.js
@@ -1,21 +1,26 @@
 $(document).ready(function() {
-    $('#UserEmail').on('focusout',function(){
-		$('#popup-email-warning').modal({  
-			containerId: 'gotest-email-warning',  
-			opacity: 80,  
-			closeClass: 'gotest-close',  
-			onOpen: function(e) {
-				e.data.show();
-				e.container.show();
-				e.overlay.fadeIn();
-			},
-			onClose: function(e) {
-				e.data.hide();
-				e.container.hide();
-				e.overlay.fadeOut(function(){
-					$.modal.close();
-				})
-            }
-		});
-	});
+    $('#UserEmail').on('focusout', function() {
+        console.log($('#UserEmail').val());
+        var filter = /^([\w-\.]+@(?!mail.ru)(?!inbox.ru)(?!list.ru)(?!bk.ru)([\w-]+\.)+[\w-]{2,4})?$/
+        console.log(filter.test($('#UserEmail').val()));
+        if (!filter.test($('#UserEmail').val())) {
+            $('#popup-email-warning').modal({
+                containerId: 'gotest-email-warning',
+                opacity: 80,
+                closeClass: 'gotest-close',
+                onOpen: function(e) {
+                    e.data.show();
+                    e.container.show();
+                    e.overlay.fadeIn();
+                },
+                onClose: function(e) {
+                    e.data.hide();
+                    e.container.hide();
+                    e.overlay.fadeOut(function() {
+                        $.modal.close();
+                    })
+                }
+            });
+        }
+    });
 })
