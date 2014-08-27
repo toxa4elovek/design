59ddd3341458ac3ed280e51343267bcfb4e43df8
Dmitriy Nyu <nyudmitriy@gmail.com>
2014-08-27 17:35:28 +0400

    Details - small refactor

--- app/views/pitches/details.html.php
+++ app/views/pitches/details.html.php
@@ -103,7 +103,7 @@ function mb_basename($file)
                             <?php endforeach?>
                         </ul>
                         <?php endif?>
-                      <?php if($this->session->read('user.id')):?>
+                      <?php if($this->user->isLoggedIn()):?>
                       <div class="separator" style="width: 620px; margin:30px 0 15px;"></div>
                       <div style="height:20px;margin-top:22px;"><span id="rating_brief">Оцените бриф:</span>
                         <div style="position:relative;left:100px;bottom:20px;" id="pitch_rating" data-pitchid="<?=$pitch->id?>" data-read="<?php echo $this->session->read('user.id') ? 'false' : 'true' ?>" data-rating="<?=$rating?>"></div>
