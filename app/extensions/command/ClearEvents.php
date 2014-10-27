75165bb9b04283d33fe29fc038b27c83c30d4434
Dmitriy Nyu <nyudmitriy@gmail.com>
2014-10-27 13:52:05 +0300

    Feed - fix command

--- app/extensions/command/ClearEvents.php
+++ app/extensions/command/ClearEvents.php
@@ -3,11 +3,13 @@
 namespace app\extensions\command;
 
 use app\models\Event;
+use app\extensions\storage\Rcache;
 
 class ClearEvents extends \app\extensions\command\CronJob {
 
     public function run() {
-        $events = Event::all(array('order' => array('created' => 'asc'), 'limit' => 100));
+        Rcache::init();
+        $events = Event::all(array('order' => array('created' => 'desc'), 'limit' => 100));
         foreach ($events as $event) {
             if ($event->type == 'SolutionAdded' && !$event->solution) {
                 $event->delete();
