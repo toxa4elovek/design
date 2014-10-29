6e0a4ea6456cc4d3ebd6f62568a641f9469f03fa
Влад <vladbar.job@yandex.ru>
2014-10-29 18:40:23 +1000

    Users - com for change users gender

---
+++ app/extensions/command/ChangeUserGender.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\extensions\command;
+
+use \app\models\User;
+
+class ChangeUserGender extends \app\extensions\command\CronJob {
+
+    public function run() {
+        $users = User::all(array('conditions' => array('facebook_uid' => array('!=' => ''), 'gender' => 0), 'order' => array('lastTimeOnline' => 'desc'), 'limit' => 100));
+        $count = count($users);
+        $url = 'http://graph.facebook.com/?ids=';
+        $x = 0;
+        foreach ($users as $user) {
+            ++$x;
+            $url .= $count > $x ? $user->facebook_uid . ',' : $user->facebook_uid;
+        }
+        $json = file_get_contents($url);
+        $fusers = json_decode($json);
+        foreach ($fusers as $f) {
+            $gender = 0;
+            if ($f->gender === 'male') {
+                $gender = 1;
+            } elseif ($f->gender === 'female') {
+                $gender = 2;
+            }
+            foreach ($users as $user) {
+                if ((int) $f->id === (int) $user->facebook_uid) {
+                    $user->gender = $gender;
+                    break;
+                }
+            }
+        }
+        $users->save();
+    }
+
+}
