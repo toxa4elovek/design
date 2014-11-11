bd6dabf61eeac89f4e0ef7f9ce8006baa91c106e
Dmitriy Nyu <nyudmitriy@gmail.com>
2014-11-12 00:15:42 +0100

    Dev - fix link

--- app/views/elements/header.html.php
+++ app/views/elements/header.html.php
@@ -219,7 +219,7 @@
                 <?php endif?>
             </div>
             <ul class="header-menu">
-                <li class="header-menu-item"><a href="/users/feed">Обновления</a></li>
+                <li class="header-menu-item"><a href="/users/office">Обновления</a></li>
                 <li class="header-menu-item"><a href="/users/mypitches">Мои питчи</a></li>
                 <li class="header-menu-item"><a href="/users/profile">Профиль</a></li>
                 <li class="header-menu-item"><a href="/users/solutions">Решения</a></li>
