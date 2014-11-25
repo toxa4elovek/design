37740ddcb91792fe85ae7197b4041316974045c3
Dmitriy Nyu <nyudmitriy@gmail.com>
2014-11-25 12:09:29 +0100

    Feed - reset news views command rcache fix

--- app/extensions/command/ResetNewsViews.php
+++ app/extensions/command/ResetNewsViews.php
@@ -4,10 +4,12 @@ namespace app\extensions\command;
 
 use \app\models\News;
 use \app\models\Event;
+use app\extensions\storage\Rcache;
 
 class ResetNewsViews extends \app\extensions\command\CronJob {
 
     public function run() {
+        Rcache::init();
         $news = News::all();
         $post = News::getPost();
         if ($post) {
@@ -21,6 +23,7 @@ class ResetNewsViews extends \app\extensions\command\CronJob {
             $n->views = 0;
         }
         $news->save();
+        Rcache::delete('middle-post');
     }
 
 }
