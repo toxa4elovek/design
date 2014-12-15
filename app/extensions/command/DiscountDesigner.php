c098118484c71c2d9f8952056d1107e7cf9d44c1
Влад <vladbar.job@yandex.ru>
2014-12-15 18:10:46 +0900

    Emails - fix

--- app/extensions/command/DiscountDesigner.php
+++ app/extensions/command/DiscountDesigner.php
@@ -14,12 +14,14 @@ class DiscountDesigner extends \app\extensions\command\CronJob {
                         'confirmed_email' => 1), 'with' => 'Solution'));
         $count = 0;
         foreach ($users as $user) {
-            $data = array(
-                'email' => $user->email,
-                'subject' => 'Зарабатывай больше!'
-            );
-            SpamMailer::discountDesigners($data);
-            $count++;
+            if (count($user->solutions)) {
+                $data = array(
+                    'email' => $user->email,
+                    'subject' => 'Зарабатывай больше!'
+                );
+                SpamMailer::discountDesigners($data);
+                $count++;
+            }
         }
         $this->out("$count users emailed");
     }
