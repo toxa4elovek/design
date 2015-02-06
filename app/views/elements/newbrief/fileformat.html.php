6e0e540241d0bcec9a91ddbbae3bc392a6987e51
Dmitriy Nyu <nyudmitriy@gmail.com>
2015-02-06 16:35:14 +0300

    Brief - indd file format fix

--- app/views/elements/newbrief/fileformat.html.php
+++ app/views/elements/newbrief/fileformat.html.php
@@ -7,7 +7,7 @@ $defaults = array(
     '1' => array('EPS', 'JPG', 'PNG'),
     '2' => array('PSD', 'GIF', 'JPG', 'PNG'),
     '3' => array('PSD', 'JPG'),
-    '4' => array('INDD', 'TIFF', 'JPG'),
+    '4' => array('Indd', 'TIFF', 'JPG'),
     '5' => array('EPS', 'JPG'),
     '6' => array('PSD', 'EPS', 'JPG'),
     '7' => array('PDF', 'DOC', 'TXT'),
@@ -40,7 +40,7 @@ if($category->id != 7):?>
         <li class="graysupplement"><label><input type="checkbox" name="" data-value="PNG" <?php echo extensionsCheckboxChecked('PNG', $defaults, $category, $pitch);?> >.PNG</label></li>
         <li class="graysupplement"><label><input type="checkbox" name="" data-value="PDF" <?php echo extensionsCheckboxChecked('PDF', $defaults, $category, $pitch);?> >.PDF</label></li>
         <li class="wide graysupplement"><label><input type="checkbox" name="" data-value="PSD" <?php echo extensionsCheckboxChecked('PSD', $defaults, $category, $pitch);?> >.PSD (Photoshop)</label></li>
-        <li class="wide graysupplement"><label><input type="checkbox" name="" data-value="Innd" <?php echo extensionsCheckboxChecked('Innd', $defaults, $category, $pitch);?> >.Innd (In Design)</label></li>
+        <li class="wide graysupplement"><label><input type="checkbox" name="" data-value="Indd" <?php echo extensionsCheckboxChecked('Indd', $defaults, $category, $pitch);?> >.Indd (In Design)</label></li>
         <li class="graysupplement"><label><input type="checkbox" name="" data-value="GIF" <?php echo extensionsCheckboxChecked('GIF', $defaults, $category, $pitch);?> >.GIF</label></li>
         <li class="graysupplement"><label><input type="checkbox" name="" data-value="TIFF" <?php echo extensionsCheckboxChecked('TIFF', $defaults, $category, $pitch);?> >.TIFF</label></li>
         <li class="graysupplement"><label><input type="checkbox" name="" data-value="другие" <?php echo extensionsCheckboxChecked('другие', $defaults, $category, $pitch);?> >другие</label></li>
