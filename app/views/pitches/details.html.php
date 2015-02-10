c7e750851c1f83a0b55602a97fe93964bb81ce4a
Dmitriy Nyu <nyudmitriy@gmail.com>
2015-02-10 16:11:47 +0100

    Details - new share buttons

--- app/views/pitches/details.html.php
+++ app/views/pitches/details.html.php
@@ -158,17 +158,11 @@ function mb_basename($file)
                 <?php if($pitch->private == 0):?>
                 <div class="separator" style="width: 620px; margin: 30px 0 15px 0;"></div>
                 <div style="">
-                    <div style="float:left;height:20px;margin-right:15px;">
-                        <div class="fb-like" data-href="http://www.godesigner.ru/pitches/details/<?=$pitch->id?>" data-send="false" data-layout="button_count" data-width="50" data-show-faces="false" data-font="arial"></div>
-                    </div>
-                    <div style="float:left;height:20px;width:95px;">
-                        <div id="vk_like"></div>
-                    </div>
-                    <div style="float:left;height:20px;width:90px;">
-                        <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.godesigner.ru/pitches/details/<?=$pitch->id?>" data-text="Интересная задача для дизайнеров на GoDesigner" data-lang="en" data-hashtags="Go_Deer">Tweet</a>
-                    </div>
-                    <div style="float:left;height:20px;width:70px;">
-                        <div class="g-plusone" data-size="medium"></div>
+                    <div class="social-likes" data-counters="no" data-url="http://www.godesigner.ru/pitches/details/<?=$pitch->id?>">
+                        <div style="margin: 7px 0 0 9px;" class="facebook" title="Поделиться ссылкой на Фейсбуке">SHARE</div>
+                        <div style="margin: 7px 0 0 7px;" class="twitter" data-via="Go_Deer">TWITT</div>
+                        <div style="margin: 7px 0 0 7px;" class="vkontakte" title="Поделиться ссылкой во Вконтакте">SHARE</div>
+                        <div style="margin: 7px 0 0 7px;" class="pinterest" title="Поделиться картинкой на Пинтересте">PIN</div>
                     </div>
                     <div style="clear:both;width:300px;height:1px;"></div>
                 </div>
