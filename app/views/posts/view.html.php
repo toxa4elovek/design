95dd08e8739f03370eaed366312f6f6b0a0a2575
Dmitriy Nyu <nyudmitriy@gmail.com>
2015-02-13 15:41:53 +0100

    Posts - new  share buttons

--- app/views/posts/view.html.php
+++ app/views/posts/view.html.php
@@ -25,28 +25,15 @@
 
                             <div style="margin-top:50px; clear:both;height:3px; background: url(/img/sep.png) repeat-x scroll 0 0 transparent;width:640px;margin-bottom:15px;"></div>
                             <?php if($post->published == 1):?>
-                            <div style="">
-                                    <div style="float:left;height:20px;margin-right:15px;">
-                                        <div class="fb-like" data-href="http://www.godesigner.ru/posts/view/<?=$post->id?>" data-send="false" data-layout="button_count" data-width="50" data-show-faces="false" data-font="arial"></div>
-                                    </div>
-                                    <div style="float:left;height:20px;width:95px;">
-                                        <div id="vk_like"></div>
-                                    </div>
-                                    <div style="float:left;height:20px;width:90px;">
-                                        <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.godesigner.ru/posts/view/<?=$post->id?>" data-text="<?=$post->title?>" data-lang="en" data-hashtags="Go_Deer">Tweet</a>
-                                        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-                                    </div>
-                                    <div style="float:left;height:20px;width:70px;">
-                                        <div class="g-plusone" data-size="medium"></div>
-                                    </div>
-                                    <div style="float:left;height:20px;width:70px;">
-                                        <a href="http://pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.godesigner.ru%2Fposts%2Fview%2F<?=$post->id?>&media=<?=$post->imageurl?>&description=<?=$post->title?>" class="pin-it-button" count-layout="horizontal"><img border="0" src="//assets.pinterest.com/images/PinExt.png" title="Pin It" /></a>
-                                    </div>
-                                    <div style="float:left;height:20px;width:80px;">
-                                        <a target="_blank" class="surfinbird__like_button" data-surf-config="{'layout': 'common', 'width': '120', 'height': '20'}" href="http://surfingbird.ru/share">Серф</a>
+                                <div style="">
+                                    <div style="margin-top: -20px;" class="social-likes" data-counters="no" data-url="http://www.godesigner.ru/posts/view/<?=$post->id?>">
+                                        <div style="margin: 7px 0 0 9px;" class="facebook" title="Поделиться ссылкой на Фейсбуке">SHARE</div>
+                                        <div style="margin: 7px 0 0 7px;" class="twitter" data-via="Go_Deer">TWITT</div>
+                                        <div style="margin: 7px 0 0 7px;" class="vkontakte" title="Поделиться ссылкой во Вконтакте">SHARE</div>
+                                        <div style="margin: 7px 0 0 7px;" class="pinterest" title="Поделиться картинкой на Пинтересте">PIN</div>
                                     </div>
                                     <div style="clear:both;width:300px;height:1px;"></div>
-                            </div>
+                                </div>
                             <?php endif?>
                         <?php if(count($related) > 0):?>
                             <div style="margin-top:15px;height:261px;width:610px;background-image: url(/img/relatedback.png)">
@@ -102,5 +89,5 @@
     </div><!-- /middle -->
 
 </div><!-- .wrapper -->
-<?=$this->html->script(array('http://userapi.com/js/api/openapi.js?' . mt_rand(100, 999), 'http://surfingbird.ru/share/share.min.js', 'http://assets.pinterest.com/js/pinit.js','/js/fotorama/jquery-1.8.0.min.js','/js/fotorama/jquery.validate.min.js','/js/fotorama/jquery.countdown.min.js', 'jquery.timeago', '/js/fotorama/fotorama.js', 'posts/view'), array('inline' => false))?>
-<?=$this->html->style(array('/js/fotorama/fotorama.css', '/help', '/blog', 'disqus'), array('inline' => false))?>
+<?=$this->html->script(array('http://userapi.com/js/api/openapi.js?' . mt_rand(100, 999), 'http://surfingbird.ru/share/share.min.js', 'http://assets.pinterest.com/js/pinit.js','/js/fotorama/jquery-1.8.0.min.js','/js/fotorama/jquery.validate.min.js','/js/fotorama/jquery.countdown.min.js', 'jquery.timeago', '/js/fotorama/fotorama.js', 'social-likes.min.js', 'posts/view'), array('inline' => false))?>
+<?=$this->html->style(array('/js/fotorama/fotorama.css', '/help', '/blog', 'disqus', '/css/social-likes_flat'), array('inline' => false))?>
--- app/webroot/js/posts/view.js
+++ app/webroot/js/posts/view.js
@@ -62,21 +62,7 @@ $(document).ready(function() {
         }
     });
 
-    // gplus
-    window.___gcfg = {lang: 'ru'};
-
-    (function() {
-        var po = document.createElement('script');
-        po.type = 'text/javascript';
-        po.async = true;
-        po.src = 'https://apis.google.com/js/plusone.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(po, s);
-    })();
-
-    //vk like
-    VK.init({apiId: 2950889, onlyWidgets: true});
-    VK.Widgets.Like("vk_like", {type: "mini"});
+    $('.social-likes').socialLikes();
 
     (function($){
 
