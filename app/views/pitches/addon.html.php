b26b7048ac4bfe3cef14bee673d0d50b0dd5ac7c
Dmitriy Nyu <nyudmitriy@gmail.com>
2015-04-24 10:44:15 +0200

    Addon - remove payanyway

--- app/views/pitches/addon.html.php
+++ app/views/pitches/addon.html.php
@@ -120,7 +120,7 @@
                 <div class="g_line"></div>
                 <div id="P_card">
                     <table>
-                        <tr>
+                        <!--tr>
                             <td>
                                 <input type="radio" name="1" class="rb1" data-pay="payanyway">
                             </td>
@@ -148,7 +148,7 @@
 
                         <tr>
                             <td colspan="4"><div class="g_line"><i>или</i></div></td>
-                        </tr>
+                        </tr-->
 
                         <tr class="paymaster-section">
                             <td>
@@ -156,7 +156,7 @@
                             </td>
                             <td colspan="3" class="s3_text" style="padding-left: 20px;">
                                 Оплата пластиковыми картами и эл. деньгами <br>через PayMaster<br><br>
-                                <p style="font-size:11px; text-transform: ">Всвязи с временным ограничением платежной системы PayMaster,<br> максимально возможная сумма платежа может составлять от 15000-35000. <br>Подробнее <a href="/answers/view/91">тут</a>. В случае, если ваш платеж превышает лимит, пожалуйста, воспользуйтесь переводом на рассчетный счет (ниже).<br> Спасибо за понимание!</p>
+                                <!--p style="font-size:11px; text-transform: ">Всвязи с временным ограничением платежной системы PayMaster,<br> максимально возможная сумма платежа может составлять от 15000-35000. <br>Подробнее <a href="/answers/view/91">тут</a>. В случае, если ваш платеж превышает лимит, пожалуйста, воспользуйтесь переводом на рассчетный счет (ниже).<br> Спасибо за понимание!</p-->
                             </td>
                         </tr>
                         <tr id="paymaster-images" class="paymaster-section">
