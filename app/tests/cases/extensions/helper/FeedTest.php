4115709abcc82ed49dc034099ceb8684f528c6b3
Dmitriy Nyu <nyudmitriy@gmail.com>
2015-06-02 16:12:18 +0200

    Feed - video for backend templates #34816091242666

---
+++ app/tests/cases/extensions/helper/FeedTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\tests\cases\extensions\helper;
+
+use app\extensions\helper\Feed;
+
+class FeedTest extends \lithium\test\Unit
+{
+
+    public $feed = null;
+
+    public function setUp() {
+        $this->feed = new Feed();
+    }
+
+    public function testIsEmbeddedLink() {
+        $this->assertFalse($this->feed->isEmbeddedLink('http://www.google.com/test'));
+        $this->assertTrue($this->feed->isEmbeddedLink('https://www.youtube.com/watch?v=NifyOHkaztY'));
+        $this->assertTrue($this->feed->isEmbeddedLink('https://vimeo.com/129346968'));
+    }
+
+    /** методы стали приватными
+    public function testIsYoutubeLink() {
+        $this->assertFalse($this->feed->isYoutubeLink('http://www.google.com/test'));
+        $this->assertTrue($this->feed->isYoutubeLink('https://www.youtube.com/watch?v=NifyOHkaztY'));
+        $this->assertFalse($this->feed->isYoutubeLink('https://vimeo.com/129346968'));
+    }
+
+    public function testIsVimeoLink() {
+        $this->assertFalse($this->feed->isVimeoLink('http://www.google.com/test'));
+        $this->assertFalse($this->feed->isVimeoLink('https://www.youtube.com/watch?v=NifyOHkaztY'));
+        $this->assertTrue($this->feed->isVimeoLink('https://vimeo.com/129346968'));
+    }**/
+
+    public function testGenerateEmbeddedIframe() {
+        $expected = '<iframe width="600" height="337" src="https://www.youtube.com/embed/NifyOHkaztY" frameborder="0" allowfullscreen></iframe>';
+        $this->assertIdentical($expected, $this->feed->generateEmbeddedIframe('https://www.youtube.com/watch?v=NifyOHkaztY'));
+        $expected = '<iframe src="https://player.vimeo.com/video/129346968" width="600" height="337" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+        $this->assertIdentical($expected, $this->feed->generateEmbeddedIframe('https://vimeo.com/129346968'));
+        $expected = '<iframe width="600" height="337" src="https://www.youtube.com/embed/NifyOHkaztb" frameborder="0" allowfullscreen></iframe>';
+        $this->assertIdentical($expected, $this->feed->generateEmbeddedIframe('https://www.youtube.com/watch?v=NifyOHkaztb'));
+        $expected = '<iframe src="https://player.vimeo.com/video/129346967" width="600" height="337" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+        $this->assertIdentical($expected, $this->feed->generateEmbeddedIframe('https://vimeo.com/129346967'));
+    }
+
+}
--- app/views/users/feed.html.php
+++ app/views/users/feed.html.php
@@ -523,6 +523,10 @@
                                                 <p class="img-box">
                                                     <a class="post-link" href="<?= $object['news']['link'] ?>" target="_blank"><img onerror="imageLoadError(this);" class="img-post" src="<?= ((strpos($object['news']['imageurl'],'/events/') !== false) && (strpos($object['news']['imageurl'],'/events/') === 0)) ? 'http://www.godesigner.ru'.$object['news']['imageurl'] : $object['news']['imageurl']?>"></a>
                                                 </p>
+                                                <?php elseif($this->feed->isEmbeddedLink($object['news']['link'])):?>
+                                                <p class="img-box">
+                                                    <?php echo $this->feed->generateEmbeddedIframe($object['news']['link'])?>
+                                                </p>
                                                 <?php endif?>
                                                 <div class="r-content post-content" <?php if (!$object['news']['tags']): ?>style="padding-top: 0px;"<?php endif; ?>>
                                                     <?php if ($object['news']['tags']): ?>
