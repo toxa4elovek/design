accf9986688108a91bc2b9ea4725eec24eea8395
Dmitriy Nyu <nyudmitriy@gmail.com>
2015-06-16 15:50:44 +0200

    Brief - text fixes #20790812261674

--- app/views/elements/newbrief/pitchtitle_block.html.php
+++ app/views/elements/newbrief/pitchtitle_block.html.php
@@ -1,7 +1,7 @@
 <div class="groupc">
     <p>
         <label class="required">Название проекта <?= $this->view()->render(array('element' => 'newbrief/required_star')) ?> <!--a href="#" class="second tooltip" title="Кратко напишите, что вам необходимо создать и для какого бренда. (прим.: обёртка для шоколада “Мишка на севере”) Подробнее о брифе в разделе “Помощь”.">(?)</a-->
-            <a href="#" class="visibility-eye-tooltip tooltip" title="Название видно всем"><img src="/img/public-comment-eye.png" alt="Виден всем"></a>
+            <a href="#" class="visibility-eye-tooltip tooltip" title="Эта информация будет доступна всем, в т.ч. поисковым системам"><img src="/img/public-comment-eye.png" alt="Виден всем"></a>
         </label>
         <?php if ($category->id != 7): ?>
             <input id="pitch-title" type="text" name="title" placeholder="<?= $word1 ?> для Star Lift" data-placeholder="<?= $word1 ?> для Star Lift" value="<?php if($pitch): echo $pitch->title; endif;?>" required>
