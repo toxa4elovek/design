b87802e3ea01923fea8137f9f00919502b4ecfaa
Dmitriy Nyu <nyudmitriy@gmail.com>
2015-08-20 10:15:58 +0200

    Refactor - improvments to name inflector and bug fix

--- app/extensions/helper/NameInflector.php
+++ app/extensions/helper/NameInflector.php
@@ -12,7 +12,7 @@ namespace app\extensions\helper;
 class NameInflector extends \lithium\template\Helper {
 
 	static function renderName($first, $second = '') {
-		if(preg_match('@\W\s\W@', $first)) {
+		if(strpos(trim($first), ' ')) {
 			$exploded = explode(' ', $first);
 			$first = $exploded[0];
 			if(empty($second)) {
@@ -20,6 +20,11 @@ class NameInflector extends \lithium\template\Helper {
 			}
 		}
 
+		if(strpos(trim($second), ' ') && empty($first)) {
+			$exploded = explode(' ', $second);
+			$first = $exploded[0];
+			$second = $exploded[1];
+		}
 
 	    $dot = ($second == '') ? '' : '.';
 		return strip_tags($first . ' ' . mb_substr($second, 0, 1, 'utf-8') . $dot);
