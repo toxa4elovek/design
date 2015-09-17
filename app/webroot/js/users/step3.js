4049e8d775869e6010ca1c9197c064497dd587a4
Dmitriy Nyu <nyudmitriy@gmail.com>
2015-09-17 15:12:52 +0200

    Refactor - bugfix

--- app/webroot/js/users/step3.js
+++ app/webroot/js/users/step3.js
@@ -1,25 +1,27 @@
-$(document).ready(function() {
+;(function($) {
+    $(function () {
 
-    $('#nofile').click(function() {
-        $('#nofiles-warning').modal({
-            containerId: 'generic-popup',
-            opacity: 80,
-            closeClass: 'popup-close'
+        $('#nofile').click(function () {
+            $('#nofiles-warning').modal({
+                containerId: 'generic-popup',
+                opacity: 80,
+                closeClass: 'popup-close'
+            });
+            return false;
         });
-        return false;
+
+        $('#confirm').click(function () {
+            $('#important-confirm').modal({
+                containerId: 'generic-popup',
+                opacity: 80,
+                closeClass: 'popup-close'
+            });
+            return false;
+        });
+
+        $('#confirmWinner').click(function () {
+            window.location = ($('#confirm').attr('href'));
+        });
+
     });
-
-    $('#confirm').click(function(){
-        $('#important-confirm').modal({
-            containerId: 'generic-popup',
-            opacity: 80,
-            closeClass: 'popup-close'
-        });
-        return false;
-    })
-
-    $('#confirmWinner').click(function() {
-        window.location = ($('#confirm').attr('href'));
-    })
-
-})
+}) ($);
