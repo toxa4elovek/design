c98bfd96d4706d0c3dfea9ce03d8c4a15fa7ef3b
Dmitriy Nyu <nyudmitriy@gmail.com>
2015-09-18 15:13:32 +0200

    Closing - bug fix and text change

--- app/webroot/js/users/step4.js
+++ app/webroot/js/users/step4.js
@@ -1,17 +1,22 @@
-$(document).ready(function() {
+;(function($) {
 
-    $( ".slider" ).slider({
-        value: 5,
-        min: 0,
-        max: 5,
-        step: 1,
-        slide: function( event, ui ) {
-            $('input[name=' + $(this).data("field") +']').val(ui.value);
-        }
-    })
+    $(function() {
 
-    $('.fakeclick').click(function() {
-        $('#afterPitchCommentForm').submit();
-        return false;
-    })
-})
+        $( ".slider" ).slider({
+            value: 5,
+            min: 0,
+            max: 5,
+            step: 1,
+            slide: function( event, ui ) {
+                $('input[name=' + $(this).data("field") +']').val(ui.value);
+            }
+        })
+
+        $('.fakeclick').click(function() {
+            $('#afterPitchCommentForm').submit();
+            return false;
+        });
+
+    });
+
+}) ($);
