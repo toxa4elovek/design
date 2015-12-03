cb42d496c204f39255a7e5c4570af33559135b39
Dmitriy Nyu <nyudmitriy@gmail.com>
2015-12-03 23:54:34 +0100

    Bugs - added static keyword for clearOldAvatar method

--- app/models/Avatar.php
+++ app/models/Avatar.php
@@ -2,17 +2,28 @@
 
 namespace app\models;
 
-class Avatar extends AppModel {
+/**
+ * Class Avatar
+ *
+ * Метод для управления аватарами пользователя
+ * @package app\models
+ */
+class Avatar extends AppModel
+{
 
-    public function clearOldAvatars($userId) {
-        $avatars = self::all(array('conditions' => array('model_id' => $userId)));
-        foreach($avatars as $avatar) {
-            if(file_exists($avatar->filename)) {
+    /**
+     * Метод удаляет все записи и файлы аватаров для пользователя
+     *
+     * @param $userId
+     */
+    public static function clearOldAvatars($userId)
+    {
+        $avatars = self::all(['conditions' => ['model_id' => $userId]]);
+        foreach ($avatars as $avatar) {
+            if (file_exists($avatar->filename)) {
                 unlink($avatar->filename);
             }
             $avatar->delete();
         }
     }
-
-
 }
