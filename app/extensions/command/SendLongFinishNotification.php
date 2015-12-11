c63135ed878e2523cf1fedd9cae33e6f04385afa
Dmitriy Nyu <nyudmitriy@gmail.com>
2015-12-11 09:16:08 +0100

    Bugs - command namespace fix

--- app/extensions/command/SendLongFinishNotification.php
+++ app/extensions/command/SendLongFinishNotification.php
@@ -2,12 +2,22 @@
 
 namespace app\extensions\command;
 
-use \app\models\Pitch;
 use app\extensions\mailers\NotificationsMailer;
+use app\models\Pitch;
+use app\models\User;
 
-class SendLongFinishNotification extends CronJob {
+/**
+ * Class SendLongFinishNotification
+ * @package app\extensions\command
+ *
+ * Команда отправляет письмо, если завершения слишком долгое
+ *
+ */
+class SendLongFinishNotification extends CronJob
+{
 
-    public function run() {
+    public function run()
+    {
         $this->header('Welcome to the SendLongFinishNotification command!');
         $firstDate = date('Y-m-d H:i:s', time() - 14 * DAY);
         $secondDate = date('Y-m-d H:i:s', time() - 15 * DAY);
@@ -18,8 +28,8 @@ class SendLongFinishNotification extends CronJob {
             'awarded' => array('!=' => 0),
             "awardedDate <= '$firstDate' AND awardedDate > '$secondDate'"
         )));
-        foreach($projects as $project) {
-            $user = User::first(32);
+        foreach ($projects as $project) {
+            $user = User::first(5);
             NotificationsMailer::sendLongFinishNotification($user, $project);
         }
     }
