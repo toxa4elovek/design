71449ce2cb16f318b474441a430b76269acb1b79
DmitriySpb <nyudmitriy@gmail.com>
2016-02-20 12:53:49 +0300

    Push notifications - new notification about best solution

--- app/extensions/command/BestSolutionInTwitter.php
+++ app/extensions/command/BestSolutionInTwitter.php
@@ -3,10 +3,13 @@
 namespace app\extensions\command;
 
 use app\models\Event;
+use app\models\Pitch;
 use app\models\Solution;
 use app\extensions\social\SocialMediaManager;
 use app\extensions\social\TwitterAPI;
 use app\extensions\storage\Rcache;
+use OneSignal\Config;
+use OneSignal\OneSignal;
 
 class BestSolutionInTwitter extends CronJob {
 
@@ -50,6 +53,34 @@ class BestSolutionInTwitter extends CronJob {
                     $tweetsDump[$id] = $embeddedData['html'];
                     Rcache::write('RetweetsFeed', $tweetsDump);
                 }
+                $project = Pitch::first($solution->pitch_id);
+                $config = new Config();
+                $config->setApplicationId('46001cba-49be-4cc5-945a-bac990a6d995');
+                $config->setApplicationAuthKey('YTRkYWE2OWMtNjQ4OS00ZjI1LThiZjItZjVlMzdlMWM2Mzc2');
+                $config->setUserAuthKey('YmFjYWI1MTQtYjgzOS00NDFhLTg2YjAtY2IzZjc4OWFjNGVm');
+                $api = new OneSignal($config);
+                $api->notifications->add([
+                    'contents' => [
+                        'en' => 'Самое популярное решение за' . date('d.m.Y', $lastDay),
+                        'ru' => 'Самое популярное решение за' . date('d.m.Y', $lastDay)
+                    ],
+                    'headings' => [
+                        'en' => $mediaManager->getProjectTitleForSocialNetwork($project),
+                        'ru' => $mediaManager->getProjectTitleForSocialNetwork($project),
+                    ],
+                    'included_segments' => ['All'],
+                    'url' => 'https://www.godesigner.ru/pitches/viewsolution/' . $solution->id,
+                    'isChromeWeb' => true,
+                ]);
+                $api->notifications->add([
+                    'contents' => [
+                        'en' => 'Самое популярное решение за' . date('d.m.Y', $lastDay) ,
+                        'ru' => 'Самое популярное решение за' . date('d.m.Y', $lastDay)
+                    ],
+                    'included_segments' => ['All'],
+                    'url' => 'https://www.godesigner.ru/pitches/viewsolution/' . $solution->id,
+                    'isSafari' => true,
+                ]);
                 $this->out('Event saved');
                 $this->out('The best solution for ' . $day . ' sent');
             } else {
