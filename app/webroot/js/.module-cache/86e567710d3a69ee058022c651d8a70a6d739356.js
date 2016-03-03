dd2959e173f13317a61746b19d0580ebb8271889
DmitriySpb <nyudmitriy@gmail.com>
2016-03-03 13:22:40 +0300

    BUgs - fixed untracked files

--- app/webroot/js/.module-cache/86e567710d3a69ee058022c651d8a70a6d739356.js
+++
@@ -1,6 +0,0 @@
-var FaqQuestionRow = React.createClass({displayName: "FaqQuestionRow",
-    render: function() {
-        var link = "http://www.godesigner.ru/answers/view/" + object.id;
-        return (React.createElement("li", null, React.createElement("a", {href: link}, object.title)))
-    },
-});
