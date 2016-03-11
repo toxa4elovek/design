4b1abba81aa8a8309e6f282b1ad6a7b0c9483797
DmitriySpb <nyudmitriy@gmail.com>
2016-03-11 13:43:31 +0300

    Bugs - fix website qualities

--- app/views/elements/brief-edit/3.html.php
+++ app/views/elements/brief-edit/3.html.php
@@ -1,11 +1,4 @@
 <div class="groupc">
-<p>
-    <label>Какие 3 основных качества нужно донести до аудитории?<a href="#" class="visibility-eye-tooltip tooltip private" title="Эта информация будет доступна вам и участникам, которые подписали соглашение о неразглашении"><img src="/img/private-comment-eye.png" alt="Информация скрыта"></a></label>
-    <input type="text" name="qualities" placeholder="Прагматичный, надежный, элегантный" value="<?=$specifics['qualities']?>" class="specific-prop">
-</p>
-</div>
-
-<div class="groupc">
 
     <p style="margin-top: 24px; margin-bottom: 38px;">
         <label><a href="#" class="expand_extra">+ Дополнительная информация</a><a href="#" class="visibility-eye-tooltip tooltip private" title="Эта информация будет доступна вам и участникам, которые подписали соглашение о неразглашении"><img src="/img/private-comment-eye.png" alt="Информация скрыта"></a></label>
