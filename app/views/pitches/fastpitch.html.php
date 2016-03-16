88852f37cd174a08542841d9143eac126778d36d
DmitriySpb <nyudmitriy@gmail.com>
2016-03-16 14:06:34 +0300

    Payments - logosale remove paymaster and add wire transfer

--- app/views/pitches/fastpitch.html.php
+++ app/views/pitches/fastpitch.html.php
@@ -74,13 +74,71 @@
                         <tr>
                             <td colspan="4"><div class="g_line"><i>или</i></div></td>
                         </tr>
-                        <tr class="paymaster-section">
+                        <tr>
+                            <td>
+                                <input type="radio" name="1" class="rb1" data-pay="offline">
+                            </td>
+                            <td class="s3_h">
+                                <img src="/img/s3_rsh.png" alt="">
+                            </td>
+                            <td class="s3_text">
+                                Перевод  на расчетный счёт<br/>(Безналичный платеж через банк)
+                            </td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td colspan="4">
+                                <div id="s3_kv">
+                                    <label><input type="radio" name="radio-face" class="rb-face" data-pay="offline-fiz"> ФИЗИЧЕСКОЕ ЛИЦО</label>
+                                    <label><input type="radio" name="radio-face" class="rb-face" data-pay="offline-yur"> ЮРИДИЧЕСКОЕ ЛИЦО</label>
+                                    <div class="pay-fiz">
+                                        <p>Заполните поля, скачайте счёт на оплату и оплатите его. С помощью него вы можете сделать безналичный перевод через банк.</p>
+                                        <form action="/bills/save" method="post" id="bill-fiz">
+                                            <input type="hidden" name="fiz-id" id="fiz-id" value="<?=$pitch->id?>">
+                                            <input type="hidden" name="fiz-individual" id="fiz-individual" value="1">
+                                            <input type="text" name="fiz-name" id="fiz-name" placeholder="Иванов Иван Иванович" data-placeholder="Иванов Иван Иванович" required="" data-content="symbolic">
+                                            <img src="/img/arrow-bill-download.png" class="arrow-bill-download" />
+                                            <input type="submit" id="button-fiz" value="Скачать счёт" class="button third" style="width:420px;">
+                                            <div class="clr"></div>
+                                        </form>
+                                        <p>Мы активируем ваш проект на сайте в течение рабочего дня после поступления денег, и тогда он появится в <a href="/pitches">общем списке</a>.
+                                            Пока вы можете просмотреть ваш проект в <a href="/users/mypitches">личном кабинете</a>.</p>
+                                    </div>
+                                    <div class="pay-yur">
+                                        <p>Заполните поля, скачайте счёт на оплату и оплатите его. С помощью него вы можете сделать безналичный перевод через банк.</p>
+                                        <form action="/bills/save" method="post" id="bill-yur">
+                                            <input type="hidden" name="yur-id" id="yur-id" value="<?=$pitch->id?>">
+                                            <input type="hidden" name="yur-individual" id="yur-individual" value="0">
+
+                                            <label class="required">Наименование организации</label>
+                                            <input type="text" name="yur-name" id="yur-name" placeholder="OOO «КРАУД МЕДИА»" data-placeholder="OOO «КРАУД МЕДИА»" required="" data-content="mixed">
+
+                                            <label class="required">ИНН</label>
+                                            <input type="text" name="yur-inn" id="yur-inn" placeholder="123456789012" data-placeholder="123456789012" required="" data-content="numeric" data-length="[10,12]">
+
+                                            <label>КПП</label>
+                                            <input type="text" name="yur-kpp" id="yur-kpp" placeholder="123456789" data-placeholder="123456789" required="" data-content="numeric" data-length="[9]">
+
+                                            <label class="required">Юридический адрес</label>
+                                            <input type="text" name="yur-address" id="yur-address" placeholder="199397, Санкт-Петербург, ул. Беринга, д. 27" data-placeholder="199397, Санкт-Петербург, ул. Беринга, д. 27" required="" data-content="mixed">
+
+                                            <p>Мы свяжемся с вами в течение рабочего дня после поступления денег и договоримся об удобном времени для заполнения брифа.</p>
+                                            <p>Закрывающие документы вы получите на e-mail сразу после того, как завершите проект. Распечатайте их, подпишите и поставьте печать.
+                                                Отправьте их нам в двух экземплярах по почте (199397, Россия, Санкт-Петербург, ул. Беринга, д. 27).
+                                                В ответном письме вы получите оригиналы документов с нашей печатью.</p>
+                                            <input type="submit" id="button-yur" value="Скачать счёт" class="button third" style="width:420px;">
+                                            <div class="clr"></div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <!--tr class="paymaster-section">
                             <td>
                                 <input type="radio" name="1" class="rb1" data-pay="paymaster" style="background: #a2b2bb;">
                             </td>
                             <td colspan="1" class="s3_text" style="padding-left: 20px;">
-                                Оплата пластиковыми картами и эл. деньгами <!--br-->через PayMaster<br><br>
-                                <!--p style="font-size:11px; text-transform: ">Всвязи с временным ограничением платежной системы PayMaster,<br> максимально возможная сумма платежа может составлять от 15000-35000. <br>Подробнее <a href="/answers/view/91">тут</a>. В случае, если ваш платеж превышает лимит, пожалуйста, воспользуйтесь переводом на рассчетный счет (ниже).<br> Спасибо за понимание!</p-->
+                                Оплата пластиковыми картами и эл. деньгами через PayMaster<br><br>
                             </td>
                         </tr>
                         <tr id="paymaster-images" class="paymaster-section">
@@ -94,7 +152,7 @@
                                 <?php echo $this->html->script(array('jquery-1.8.3.min.js')); ?>
                                 <script type='text/javascript' src='https://paymaster.ru/widget/BasicFP/1?LMI_MERCHANT_ID=d5d2e177-6ed1-4e5f-aac6-dd7ea1c16f60&LMI_PAYMENT_AMOUNT=<?= $sum ?>&LMI_PAYMENT_DESC=<?php echo urlencode('Оплата проекта') ?>&LMI_CURRENCY=RUB&LMI_PAYMENT_NO=<?= $pitch->id ?>'></script>
                             </td>
-                        </tr>
+                        </tr-->
                     </table>
                 </div>
                 <div class="g_line"></div>
