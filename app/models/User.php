2f5fcc78d5b51ad95176f210b752ce9dde2ab6e7
DmitriySpb <nyudmitriy@gmail.com>
2016-04-08 15:15:03 +0300

    Users - new blog author

--- app/models/User.php
+++ app/models/User.php
@@ -66,7 +66,7 @@ class User extends AppModel
      *
      * @var array
      */
-    public static $authors = array(8472, 18856, 25252, 30454);
+    public static $authors = array(8472, 18856, 25252, 30454, 34461);
 
     /**
      * Массив хранящий айдишнки авторов ленты новостей
--- app/webroot/js/fileman/php/security.inc.php
+++ app/webroot/js/fileman/php/security.inc.php
@@ -23,7 +23,7 @@
 function checkAccess($action){
     session_name('godesigner');
     session_start();
-    if((isset($_SESSION['user'])) && (isset($_SESSION['user']['id'])) && (in_array($_SESSION['user']['id'], array(32, 4, 5, 108, 81, 17865, 30454)))) {
+    if((isset($_SESSION['user'])) && (isset($_SESSION['user']['id'])) && (in_array($_SESSION['user']['id'], array(32, 4, 5, 108, 81, 17865, 30454, 34461)))) {
         return true;
     }
     die();
