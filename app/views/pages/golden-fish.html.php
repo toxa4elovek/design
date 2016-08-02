d4cf3bbddb4092990ef124b28aa0dcd08135f737
DmitriySpb <nyudmitriy@gmail.com>
2016-08-02 19:10:24 +0300

    Добавлен блок Ваши возможности

--- app/views/pages/golden-fish.html.php
+++ app/views/pages/golden-fish.html.php
@@ -24,26 +24,38 @@
             <ul>
                 <li class="time-place">
                     <img src="/img/pages/golden-fish/Icon_I.gif" alt="Любое время и место работы">
-                    <strong>Любое время и место работы</strong><br>
+                    <strong>Любое время и место работы</strong>
                     Количество прибыли зависит<br>
                     только от вашей занятости</li>
                 <li class="ideas">
                     <img src="/img/pages/golden-fish/Icon_II.gif" alt="100 идей на проект">
-                    <strong>100 идей на проект</strong><br>
+                    <strong>100 идей на проект</strong>
                     От 500 рублей и пары часов</li>
                 <li class="profit">
                     <img src="/img/pages/golden-fish/Icon_III.gif" alt="Неограниченная прибыль">
-                    <strong>Неограниченная прибыль</strong><br>
+                    <strong>Неограниченная прибыль</strong>
                     Проекты с прибылью<br>
                     до десятков тысяч рублей</li>
                 <li class="guarantee">
                     <img src="/img/pages/golden-fish/Icon_IV.gif" alt="Сохранность денег">
-                    <strong>Сохранность денег</strong><br>
+                    <strong>Сохранность денег</strong>
                     Если вам не нравится<br>
                     результат - деньги автоматически<br>
                     вернутся к вам на счет</li>
             </ul>
             <div class="clear"></div>
+        </section>
+        <section class="lp-how-to-profit">
+            <h2>Как вы заработаете?</h2>
+            <div class="header-profit"><img src="/img/pages/golden-fish/icons-with-lines.png" alt="Иконки"></div>
+            <ul>
+                <li class="publish"><strong>Публикуйте задачи клиента</strong>
+                    Мы не берем сервисный сбор в течение года, вы оплачиваете только гонорар дизайнеру и копирайтеру.</li>
+                <li class="choose"><strong>Выбирайте из ~100 креативных идей</strong>
+                    С нами вы получите ~100 идей на логотип и ~240 идей на нейминг в каждом проекте, что в десятки раз больше, чем от одного фрилансера.</li>
+                <li class="sell"><strong>Продавайте идеи со своей наценкой</strong>
+                    Зарабатывайте за ведение проекта на разнице: бюджет проекта (минус) гонорар исполнителю.</li>
+            </ul>
         </section>
     </main>
 </div>
