230b5dc9affbb68cd12b94375881dba72c7414dc
Dmitriy Nyu <nyudmitriy@gmail.com>
2016-08-22 11:15:44 +0300

    Обновлен шаблон про решение победитель

--- app/tests/cases/extensions/mailers/SolutionsMailerTest.php
+++ app/tests/cases/extensions/mailers/SolutionsMailerTest.php
@@ -5,36 +5,44 @@ namespace app\tests\cases\extensions\mailers;
 use app\extensions\tests\AppUnit;
 use \app\extensions\mailers\SolutionsMailer;
 
-class SolutionsMailerTest extends  AppUnit {
+class SolutionsMailerTest extends  AppUnit
+{
 
-    public function setUp() {
-        $this->rollUp(array('Pitch', 'User', 'Solution', 'Category'));
+    public function setUp()
+    {
+        $this->rollUp(['Pitch', 'User', 'Solution', 'Category']);
     }
 
-    public function tearDown() {
-        $this->rollDown(array('Pitch', 'User', 'Solution', 'Category'));
+    public function tearDown()
+    {
+        $this->rollDown(['Pitch', 'User', 'Solution', 'Category']);
     }
 
-    public function testSendNewSolutionNotification() {
+    public function testSendNewSolutionNotification()
+    {
         $html = SolutionsMailer::sendNewSolutionNotification(1);
         $this->assertPattern("/ДОБАВЛЕНО НОВОЕ РЕШЕНИЕ/", $html);
         $this->assertPattern("/ДМИТРИЙ/", $html);
         $this->assertPattern("/TEST TITLE/", $html);
     }
 
-    public function testSendVictoryNotification() {
+    public function testSendVictoryNotification()
+    {
         $html = SolutionsMailer::sendVictoryNotification(1);
-        $this->assertPattern("/ВАШЕ РЕШЕНИЕ СТАЛО ПОБЕДИТЕЛЕМ!/", $html);
-        $this->assertPattern("/АЛЕКСЕЙ/", $html);
-        $this->assertPattern("/TEST TITLE/", $html);
+        $this->assertPattern("/Ваше решение/", $html);
+        $this->assertPattern("/стало победителем\./", $html);
+        $this->assertPattern("/Здравствуйте, Алексей!<br>/", $html);
+        $this->assertPattern("/Мы поздравляем вас!/", $html);
+        $this->assertPattern("/У заказчика есть право на внесение 3 правок на/", $html);
+        $this->assertPattern("/, пожалуйста, ознакомьтесь с /", $html);
         $this->assertPattern("/#5/", $html);
     }
 
-    public function testSendSolutionBoughtNotification() {
+    public function testSendSolutionBoughtNotification()
+    {
         $html = SolutionsMailer::sendSolutionBoughtNotification(1);
         $this->assertPattern("/Ваше решение/", $html);
         $this->assertPattern("/Проверка названия/", $html);
         $this->assertPattern("/#5/", $html);
     }
-
 }
