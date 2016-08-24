753ed54307efbce18a8230f5bc66108e114d8b79
DmitriySpb <nyudmitriy@gmail.com>
2016-08-24 11:17:36 +0300

    Добавлена информация для распродажи

--- app/views/users/step2.html.php
+++ app/views/users/step2.html.php
@@ -24,11 +24,12 @@
                 <?php if(($solution->pitch->blank == 1) && ($type == 'designer')  &&  ($solution->pitch->confirmed == 0)):?>
                     <div class="regular" style="text-align: center;">
                         Та-дам! Посетитель платформы предлагает выкупить ваше решение <a style="text-decoration: none;color: #6990a0;" href="https://www.godesigner.ru/pitches/viewsolution/<?= $solution->id?>">#<?= $solution->num?></a> для<br> проекта <a style="text-decoration: none;color: #6990a0;" href="https://www.godesigner.ru/pitches/view/<?=$solution->pitch->id?>">«<?= $solution->pitch->title?>»</a> в рамках <a style="text-decoration: none;color: #6990a0;" href="https://www.godesigner.ru/logosale">распродажи логотипов</a>,
-                        а мы поздравляем<br> вас с возможностью заработать 6000р. В случае отказа, мы вернем ему деньги.<br> На подтверждение запроса у вас есть:
+                        а мы поздравляем<br> вас с возможностью заработать 6000р. В случае отказа, мы вернем ему деньги.<br> На подтверждение* запроса у вас есть:
                     </div>
                     <h3 style="font-size: 20px; color: #60879c; text-align: center; margin-top: 40px;" class="countdown" data-deadline="<?=(strtotime($solution->pitch->started)) + 3 * DAY;?>"><?php echo ($interval = $this->pitch->confirmationTimeRemain($solution->pitch)) ? $interval->format('%d дн. %H:%I:%S') : ''; ?></h3>
                     <a href="/pitches/accept/<?=$solution->pitch->id?>" style="margin-left: auto; margin-right: auto; width: 220px;display: block; margin-top: 32px" class="button">Подтвердить</a>
                     <a href="/pitches/decline/<?=$solution->pitch->id?>" data-title="<?=$solution->pitch->title?>" data-solutionid="<?=$solution->pitch->awarded?>" data-solutionnum="<?=$solution->num?>" data-pitchid="<?=$solution->pitch->id?>" class="popup-decline" style="text-shadow: -1px 0 0 #FFFFFF; margin-left: auto; margin-right: auto; width: 220px;display: block; margin-top: 9px; color: #666666; font-size: 14px; text-align: center; text-decoration: underline;">Отказать</a>
+                    <p class="regular" style="margin-top: 40px;">Подтверждение означает согласие на доработку макетов: вы обязуетесь внести в исходный макет 3 правки, включая адаптацию названия</p>
                 <?php else: ?>
                 <?php if ($solution->pitch->category_id == 7):?>
                     <?php if($type == 'designer'):?>
