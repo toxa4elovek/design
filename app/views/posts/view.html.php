21d11e9db68b36e22afb8e7a3268465068d35380
DmitriySpb <nyudmitriy@gmail.com>
2016-09-20 13:08:59 +0300

    Исправление отображаемой картинки для пинтерест

--- app/views/posts/view.html.php
+++ app/views/posts/view.html.php
@@ -1,6 +1,6 @@
 <div class="wrapper">
 
-    <?=$this->view()->render(array('element' => 'header'), array('logo' => 'logo'))?>
+    <?=$this->view()->render(['element' => 'header'], ['logo' => 'logo'])?>
 
     <div class="middle">
         <div class="middle_inner" style="padding-top: 42px;">
@@ -12,7 +12,7 @@
                         <?php
                         $tags = explode('|', $post->tags);
                         $tagstring = '';
-                        foreach($tags as $tag):
+                        foreach ($tags as $tag):
                             $tagstring[] = '<a class="blogtaglink" href="/posts?tag=' . urlencode($tag) . '">' . $tag . '</a>';
                         endforeach;
                         ?>
@@ -20,13 +20,13 @@
                             &bull; Просмотров: <?=$post->views?>
                             </p>
 
-                            <?php if($post->published == 1):?>
+                            <?php if ($post->published == 1):?>
                                 <div style="margin-top: 25px;">
                                     <div style="margin-top: -20px;" class="social-likes" data-zeroes="yes" data-counters="yes" data-url="https://www.godesigner.ru/posts/view/<?=$post->id?>">
                                         <div style="margin: 7px 0 0 9px;" class="facebook" title="Поделиться ссылкой на Фейсбуке">SHARE</div>
                                         <div style="margin: 7px 0 0 7px;" class="twitter" data-via="Go_Deer">TWITT</div>
                                         <div style="margin: 7px 0 0 7px;" class="vkontakte" data-image="<?=$post->imageurl?>" title="Поделиться ссылкой во Вконтакте">SHARE</div>
-                                        <div style="margin: 7px 0 0 7px;" class="pinterest" data-media="<?=$post->imageurl?>" title="Поделиться картинкой на Пинтересте">PIN</div>
+                                        <div style="margin: 7px 0 0 7px;" class="pinterest" data-media="https://godesigner.ru<?=$post->imageurl?>" title="Поделиться картинкой на Пинтересте">PIN</div>
                                     </div>
                                     <div style="clear:both;width:300px;height:1px;"></div>
                                 </div>
@@ -38,7 +38,7 @@
                             </div>
 
                             <div style="margin-top:50px; clear:both;height:3px; background: url(/img/sep.png) repeat-x scroll 0 0 transparent;width:640px;margin-bottom:15px;"></div>
-                            <?php if($post->published == 1):?>
+                            <?php if ($post->published == 1):?>
                                 <div style="">
                                     <div style="margin-top: -20px;" class="social-likes" data-zeroes="yes" data-counters="yes" data-url="https://www.godesigner.ru/posts/view/<?=$post->id?>">
                                         <div style="margin: 7px 0 0 9px;" class="facebook" title="Поделиться ссылкой на Фейсбуке">SHARE</div>
@@ -49,16 +49,16 @@
                                     <div style="clear:both;width:300px;height:1px;"></div>
                                 </div>
                             <?php endif?>
-                        <?php if(count($related) > 0):?>
+                        <?php if (count($related) > 0):?>
                             <div style="margin-top:15px;height:261px;width:610px;background-image: url(/img/relatedback.png)">
 <?php
                                 $i = 0;
-                                foreach($related as $relatedpost):?>
-<a class="relatedpost" href="/posts/view/<?=$relatedpost->id?>" style="padding-left:1px;height:256px;margin-top:3px;<?php if($i==2): echo 'float:right;'; else: echo 'float:left;'; endif?> <?php if($i==1):?>width: 197px;padding-left:7px;<?php else:?>width: 197px;<?php endif?>margin-right: 2px;">
-<?php if($i == 0):?>
+                                foreach ($related as $relatedpost):?>
+<a class="relatedpost" href="/posts/view/<?=$relatedpost->id?>" style="padding-left:1px;height:256px;margin-top:3px;<?php if ($i==2): echo 'float:right;'; else: echo 'float:left;'; endif?> <?php if ($i==1):?>width: 197px;padding-left:7px;<?php else:?>width: 197px;<?php endif?>margin-right: 2px;">
+<?php if ($i == 0):?>
     <h2 style="margin-top: 16px;font-weight: bold; color:#999999;font-size:17px;text-shadow: 1px 0 1px #FFFFFF;">Вам будет интересно:</h2>
                                 <?php endif?>
-    <div style="width:191px;height:140px;background-image: url(/img/frame-small.png);<?php if($i==0):?>margin-top:10px;<?php else:?>margin-top:44px<?php endif?>">
+    <div style="width:191px;height:140px;background-image: url(/img/frame-small.png);<?php if ($i==0):?>margin-top:10px;<?php else:?>margin-top:44px<?php endif?>">
         <img width="184" height="134" alt="" src="<?=$relatedpost->imageurl?>" style="margin-top:3px;margin-left:3px;">
     </div>
     <h2 class="title" style="padding-left:6px;padding-right:6px;margin-top:13px;font-size: 15px;text-shadow: 1px 0 1px #FFFFFF;color:#999999"><?=$relatedpost->title?></h2>
@@ -92,7 +92,7 @@
                         <input type="text" id="blog-search" name="search" value="" class="text">
                         <input type="submit" class="blog-submit" value="">
                     </form>
-                    <?=$this->view()->render(array('element' => 'posts/categories_menu'))?>
+                    <?=$this->view()->render(['element' => 'posts/categories_menu'])?>
                     <div id="current_pitch">
                         <?php echo $this->stream->renderStream(10, false);?>
                     </div>
@@ -103,5 +103,5 @@
     </div><!-- /middle -->
 
 </div><!-- .wrapper -->
-<?=$this->html->script(array('/js/fotorama/jquery-1.8.0.min.js','/js/fotorama/jquery.validate.min.js','/js/fotorama/jquery.countdown.min.js', 'jquery.timeago', '/js/fotorama/fotorama.js', 'social-likes.min.js', 'posts/view'), array('inline' => false))?>
-<?=$this->html->style(array('/js/fotorama/fotorama.css', '/help', '/blog', '/css/common/buttons.css', '/css/posts/view.css', 'disqus', '/css/social-likes_flat'), array('inline' => false))?>
+<?=$this->html->script(['/js/fotorama/jquery-1.8.0.min.js', '/js/fotorama/jquery.validate.min.js', '/js/fotorama/jquery.countdown.min.js', 'jquery.timeago', '/js/fotorama/fotorama.js', 'social-likes.min.js', 'posts/view'], ['inline' => false])?>
+<?=$this->html->style(['/js/fotorama/fotorama.css', '/help', '/blog', '/css/common/buttons.css', '/css/posts/view.css', 'disqus', '/css/social-likes_flat'], ['inline' => false])?>
