fb9bcb2ece131bb8a23ed97e0bb3ac1b08fa34b0
Dmitriy Nyu <nyudmitriy@gmail.com>
2016-09-28 12:08:35 +0300

    Исправление опечатки и пиксель из фейсбука

--- app/views/pages/goldenfish.html.php
+++ app/views/pages/goldenfish.html.php
@@ -99,7 +99,7 @@
                 <h2>Cтаньте директором<br/>
                     собственного рекламного<br/>
                     агентства</h2>
-                <p>С бизнес-планом «Золотая рыбка» вы ищите клиентов,<br>а дизайнеры и копирайтеры найдутся у нас.</p>
+                <p>С бизнес-планом «Золотая рыбка» вы ищете клиентов,<br>а дизайнеры и копирайтеры найдутся у нас.</p>
                 <div>
                     <ul class="call-to-action">
                         <li class="price">
@@ -177,6 +177,23 @@
         </section>
     </main>
 </div>
+<!-- Facebook Pixel Code -->
+<script>
+    !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+        n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+            document,'script','https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1707695879554473', {
+        em: 'insert_email_variable,'
+    });
+    fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+               src="https://www.facebook.com/tr?id=1707695879554473&ev=PageView&noscript=1"
+    /></noscript>
+<!-- DO NOT MODIFY -->
+<!-- End Facebook Pixel Code -->
 <?=$this->html->style([
     '/css/ui-lightness/jquery-ui.css',
     '/css/common/buttons.css',
