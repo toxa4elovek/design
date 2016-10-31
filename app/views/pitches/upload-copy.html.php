8257ac6986ac4c169b820f99179fa449c2b21561
Dmitriy Nyu <nyudmitriy@gmail.com>
2016-10-31 13:40:46 +0300

    Новый чекбокс для дизайнеров

--- app/views/pitches/upload-copy.html.php
+++ app/views/pitches/upload-copy.html.php
@@ -41,6 +41,10 @@
                     <label><input type="checkbox" name="tos" style="margin-right:5px;"/>Я прочитал(а) и выражаю безусловное согласие с условиями настоящего <a href="/docs/dogovor_2016.pdf" style="text-decoration: none;">конкурсного соглашения</a>.</label>
                 </div>
 
+                <div class="tos-container supplement">
+                    <label><input type="checkbox" name="rights_tos" style="margin-right: 5px; margin-bottom: 2px;"/>Я прочитал и согласен с <a href="http://www.godesigner.ru/docs/%D0%B4%D0%BE%D0%B3%D0%BE%D0%B2%D0%BE%D1%80%20%D0%BE%D1%82%D1%87%D1%83%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%BE%D1%84%D0%B5%D1%80%D1%82%D0%B0.doc" style="text-decoration: none;">договором передачи исключительных прав на произведение</a></label>
+                    <?= $this->view()->render(['element' => 'newbrief/required_star'], ['tooltipClass' => "tooltip3"]) ?>
+                </div>
 
 
                 <div class="group" style="background:none;">
