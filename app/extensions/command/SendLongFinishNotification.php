551b06067123bac86f4e0f1c76773a22ce37cb62
Dmitriy Nyu <nyudmitriy@gmail.com>
2016-11-28 16:31:15 +0300

    Новый получатель уведомлений о длинном завершении

--- app/extensions/command/SendLongFinishNotification.php
+++ app/extensions/command/SendLongFinishNotification.php
@@ -19,17 +19,17 @@ class SendLongFinishNotification extends CronJob
     public function run()
     {
         $this->header('Welcome to the SendLongFinishNotification command!');
-        $firstDate = date('Y-m-d H:i:s', time() - 14 * DAY);
-        $secondDate = date('Y-m-d H:i:s', time() - 15 * DAY);
-        $projects = Pitch::all(array('conditions' => array(
+        $firstDate = date(MYSQL_DATETIME_FORMAT, time() - 14 * DAY);
+        $secondDate = date(MYSQL_DATETIME_FORMAT, time() - 15 * DAY);
+        $projects = Pitch::all(['conditions' => [
             'published' => 1,
             'billed' => 1,
             'status' => 1,
-            'awarded' => array('!=' => 0),
+            'awarded' => ['!=' => 0],
             "awardedDate <= '$firstDate' AND awardedDate > '$secondDate'"
-        )));
+        ]]);
         foreach ($projects as $project) {
-            $user = User::first(108);
+            $user = User::first(47);
             NotificationsMailer::sendLongFinishNotification($user, $project);
         }
     }
