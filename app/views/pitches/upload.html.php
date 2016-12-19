731605c1930a75494f60310a0fefe5260235c8eb
Dmitriy Nyu <nyudmitriy@gmail.com>
2016-12-19 14:53:40 +0300

    Новый договор отчуждения

--- app/views/pitches/upload.html.php
+++ app/views/pitches/upload.html.php
@@ -124,7 +124,7 @@ $job_types = [
                 </div>
 
                 <div class="tos-container supplement">
-                    <label><input type="checkbox" name="rights_tos" style="margin-right: 5px; margin-bottom: 2px;"/>Я прочитал и согласен с <a href="http://www.godesigner.ru/docs/%D0%B4%D0%BE%D0%B3%D0%BE%D0%B2%D0%BE%D1%80%20%D0%BE%D1%82%D1%87%D1%83%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%BE%D1%84%D0%B5%D1%80%D1%82%D0%B0.doc" style="text-decoration: none;">договором передачи исключительных прав на произведение</a></label>
+                    <label><input type="checkbox" name="rights_tos" style="margin-right: 5px; margin-bottom: 2px;"/>Я прочитал и согласен с <a href="http://www.godesigner.ru/docs/Договор отчуждения исключительных прав.pdf" style="text-decoration: none;">договором передачи исключительных прав на произведение</a></label>
                     <?= $this->view()->render(['element' => 'newbrief/required_star'], ['tooltipClass' => "tooltip3"]) ?>
                 </div>
 
