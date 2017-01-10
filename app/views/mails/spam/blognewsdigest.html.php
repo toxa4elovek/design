34d7a91393079372c779e62579f935cb128951a7
Dmitriy Nyu <nyudmitriy@gmail.com>
2017-01-10 11:09:59 +0300

    Добавлена отписка в письмо дайджест

--- app/views/mails/spam/blognewsdigest.html.php
+++ app/views/mails/spam/blognewsdigest.html.php
@@ -9,7 +9,7 @@
     <tr>
         <td height="84">
             <div style="line-height: 0;">
-                <a href="http://godesigner.ru" target="_blank" style="border: none; text-decoration: none;"><span style="border: none; text-decoration: none;"></span><img src="http://godesigner.ru/img/mail/logo.png" alt="Go Designer" width="201" height="84" border="0" valign="top" /></a>
+                <a href="https://godesigner.ru" target="_blank" style="border: none; text-decoration: none;"><span style="border: none; text-decoration: none;"></span><img src="https://godesigner.ru/img/mail/logo.png" alt="Go Designer" width="201" height="84" border="0" valign="top" /></a>
             </div>
         </td>
     </tr>
@@ -114,9 +114,9 @@
                 <tr><td align="center">
                         <table>
                             <tr>
-                                <td width="67" align="center"><a href="https://twitter.com/Go_Deer"><img src="http://godesigner.ru/img/mail/twitter.png" alt=""></a></td>
-                                <td width="67" align="center"><a href="https://www.facebook.com/pages/Go-Designer/160482360714084"><img src="http://godesigner.ru/img/mail/facebook.png" alt=""></a></td>
-                                <td width="67" align="center"><a href="http://vk.com/godesigner"><img src="http://godesigner.ru/img/mail/vk.png" alt=""></a></td>
+                                <td width="67" align="center"><a href="https://twitter.com/Go_Deer"><img src="https://godesigner.ru/img/mail/twitter.png" alt=""></a></td>
+                                <td width="67" align="center"><a href="https://www.facebook.com/pages/Go-Designer/160482360714084"><img src="https://godesigner.ru/img/mail/facebook.png" alt=""></a></td>
+                                <td width="67" align="center"><a href="http://vk.com/godesigner"><img src="https://godesigner.ru/img/mail/vk.png" alt=""></a></td>
                             </tr>
                         </table>
                     </td></tr>
@@ -128,8 +128,8 @@
 
                 <tr><td height="8"></td></tr>
                 <tr>
-                    <td align="center" style="line-height: 13px; font-size: 9px; font-family: Arial, sans-serif;"><span style="color: #999999; text-transform: uppercase; line-height: 13px; font-size: 9px; font-family: Arial, sans-serif;">ЕСЛИ ДАННОЕ СООБЩЕНИЕ ОТОБРАЖАЕТСЯ НЕПРАВИЛЬНО, НАЖМИТЕ <a href="http://godesigner.ru/viewmail/<?= $hash ?>" target="_blank" style="color: #999999; text-decoration: underline; text-transform: uppercase; line-height: 13px; font-size: 9px; font-family: Arial, sans-serif;"><span style="color: #999999; text-decoration: underline; text-transform: uppercase; line-height: 13px; font-size: 9px; font-family: Arial, sans-serif;">ЗДЕСЬ</span></a>.<br />
-                                    <a href="http://godesigner.ru/users/profile" target="_blank" style="color: #999999; text-decoration: underline; text-transform: uppercase; line-height: 13px; font-size: 9px; font-family: Arial, sans-serif;"><span style="color: #999999; text-decoration: underline; text-transform: uppercase; line-height: 13px; font-size: 9px; font-family: Arial, sans-serif;">ОТПИСАТЬСЯ</span></a> ОТ РАССЫЛКИ.ОТПРАВЛЕНО ИЗ ГОЛОВНОГО ОФИСА GO DESIGNER, САНКТ- ПЕТЕРБУРГ, РОССИЯ.</span></td>
+                    <td align="center" style="line-height: 13px; font-size: 9px; font-family: Arial, sans-serif;"><span style="color: #999999; text-transform: uppercase; line-height: 13px; font-size: 9px; font-family: Arial, sans-serif;">ЕСЛИ ДАННОЕ СООБЩЕНИЕ ОТОБРАЖАЕТСЯ НЕПРАВИЛЬНО, НАЖМИТЕ <a href="https://godesigner.ru/viewmail/<?= $hash ?>" target="_blank" style="color: #999999; text-decoration: underline; text-transform: uppercase; line-height: 13px; font-size: 9px; font-family: Arial, sans-serif;"><span style="color: #999999; text-decoration: underline; text-transform: uppercase; line-height: 13px; font-size: 9px; font-family: Arial, sans-serif;">ЗДЕСЬ</span></a>.<br />
+                                    <a href="https://godesigner.ru/users/unsubscribe<?= $user->unsubscribeToken() ?>" target="_blank" style="color: #999999; text-decoration: underline; text-transform: uppercase; line-height: 13px; font-size: 9px; font-family: Arial, sans-serif;"><span style="color: #999999; text-decoration: underline; text-transform: uppercase; line-height: 13px; font-size: 9px; font-family: Arial, sans-serif;">ОТПИСАТЬСЯ</span></a> ОТ РАССЫЛКИ.ОТПРАВЛЕНО ИЗ ГОЛОВНОГО ОФИСА GO DESIGNER, САНКТ- ПЕТЕРБУРГ, РОССИЯ.</span></td>
                 </tr>
                 <tr><td height="13"></td></tr>
 
@@ -138,8 +138,8 @@
         </td>
     </tr>
 
-    <tr><td height="30"></td></tr>
+    <tr><td height="30"><img src="https://godesigner.ru/img/mail/grey_bottom_line.png" width="661" />
+        </td></tr>
 </table>
-<img src="http://godesigner.ru/img/mail/grey_bottom_line.png" width="661" />
 </body>
 </html>
