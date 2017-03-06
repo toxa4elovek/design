90bf22cd8e578c28c780f91f5c511decbfefce05
Dmitriy Nyu <nyudmitriy@gmail.com>
2017-03-06 16:17:05 +0300

    Исправление бага таймаута для абонентов

--- app/extensions/command/SubscribersProjectsTimeout.php
+++ app/extensions/command/SubscribersProjectsTimeout.php
@@ -2,21 +2,19 @@
 
 namespace app\extensions\command;
 
-use app\extensions\helper\User;
 use app\models\Pitch;
+use app\models\User;
 
 class SubscribersProjectsTimeout extends CronJob
 {
-
     public function run()
     {
         $this->_renderHeader();
         $currentDate = new \DateTime();
         $projectHelper = new \app\extensions\helper\Pitch();
-        $userHelper = new User();
         $subscribersProjects = Pitch::all(['conditions' => [
             'status' => 1,
-            'guaranteed' => 1,
+            'guaranteed' => 0,
             'category_id' => 20,
             'awarded' => 0,
             'chooseWinnerFinishDate' => ['<' => $currentDate->format('Y-m-d H:i:s')],
@@ -24,7 +22,7 @@ class SubscribersProjectsTimeout extends CronJob
         ]]);
         $count = 0;
         foreach ($subscribersProjects as $project) {
-            if ($project->expert == 0) {
+            if ((int) $project->expert === 0) {
                 Pitch::markAsRefunded($project->id);
                 $this->out("Project found #$project->id, marking as refunded.");
                 $count++;
@@ -33,7 +31,7 @@ class SubscribersProjectsTimeout extends CronJob
                 if (!$projectHelper->isWaitingForExperts($project)) {
                     $this->out("Project #$project->id is not waiting for experts anymore...");
                     $expertOpinionDate = $projectHelper->expertOpinion($project->id);
-                    $client = \app\models\User::first($project->user_id);
+                    $client = User::first($project->user_id);
                     if (in_array((int) $client->subscription_status, [1, 5, 6], true) === 1) {
                         $delayedChooseWinnerTime = $expertOpinionDate + (3 * DAY);
                     } elseif ((int) $client->subscription_status === 4) {
