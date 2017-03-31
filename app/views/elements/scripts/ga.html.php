66a4354629ef94905de986b06774b07647e3e60e
Dmitriy Nyu <nyudmitriy@gmail.com>
2017-03-31 17:19:25 +0300

    Обновления скрипта Hotjar

--- app/views/elements/scripts/ga.html.php
+++ app/views/elements/scripts/ga.html.php
@@ -36,14 +36,14 @@
 <!-- /Yandex.Metrika counter -->
 <!-- Hotjar Tracking Code for https://godesigner.ru/ -->
 <script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:193163,hjsv:5};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+  (function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:460790,hjsv:5};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+  })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
 </script>
 <!-- Chatra {literal} -->
 <script>
